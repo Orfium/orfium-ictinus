1fcae275d4fdd8999ca766d96c2d9fc7ca4006b3
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2020-02-11 15:09:47 +0200

    update config

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,114 +2,62 @@ import { convertPointsToPixels } from 'theme/utils';
 
 const defaultFontFamily = 'Lato';
 
-export type fontItem = {
-  fontStyle: string;
-  fontWeight: string;
-  fontSize: number;
-  fontFamily: string;
-};
-
 export type Typography = {
   globalFontSize: number;
-  font: {
-    h1: fontItem;
-    h2: fontItem;
-    h3: fontItem;
-    h4: fontItem;
-    h5: fontItem;
-    h6: fontItem;
-    h7: fontItem;
-  };
   fontSizes: {
-    xxxLarge: number;
-    xxLarge: number;
-    xLarge: number;
-    normal: number;
-    small: number;
-    xSmall: number;
-    xxSmall: number;
-    xxxSmall: number;
+    10: number;
+    11: number;
+    12: number;
+    13: number;
+    14: number;
+    15: number;
+    16: number;
+    18: number;
+    20: number;
+    22: number;
+    24: number;
+    26: number;
+    28: number;
+    32: number;
   };
   weights: {
     bold: string;
     light: string;
     regular: string;
-    bolder: string;
+    black: string;
   };
   fontFamily: string;
 };
 
 const fontSizes = {
-  xxxLarge: convertPointsToPixels(32),
-  xxLarge: convertPointsToPixels(20),
-  xLarge: convertPointsToPixels(18),
-  normal: convertPointsToPixels(16),
-  small: convertPointsToPixels(15),
-  xSmall: convertPointsToPixels(13),
-  xxSmall: convertPointsToPixels(12),
-  xxxSmall: convertPointsToPixels(11),
+  10: convertPointsToPixels(10),
+  11: convertPointsToPixels(11),
+  12: convertPointsToPixels(12),
+  13: convertPointsToPixels(13),
+  14: convertPointsToPixels(14),
+  15: convertPointsToPixels(15),
+  16: convertPointsToPixels(16),
+  18: convertPointsToPixels(18),
+  20: convertPointsToPixels(20),
+  22: convertPointsToPixels(22),
+  24: convertPointsToPixels(24),
+  26: convertPointsToPixels(26),
+  28: convertPointsToPixels(28),
+  32: convertPointsToPixels(32),
 };
 
 const weights = {
   bold: '700',
   light: '300',
   regular: '400',
-  bolder: '900',
+  black: '900',
 };
 
-export default {
+const typography: Typography = {
   globalFontSize: 16,
-  font: {
-    h1: {
-      fontStyle: 'normal',
-      fontWeight: weights.light,
-      fontSize: fontSizes.xxxLarge,
-      fontFamily: defaultFontFamily,
-    },
-
-    h2: {
-      fontStyle: 'normal',
-      fontWeight: weights.bold,
-      fontSize: fontSizes.xxLarge,
-      fontFamily: defaultFontFamily,
-    },
-
-    h3: {
-      fontStyle: 'normal',
-      fontWeight: weights.regular,
-      fontSize: fontSizes.xxLarge,
-      fontFamily: defaultFontFamily,
-    },
-
-    h4: {
-      fontStyle: 'normal',
-      fontWeight: weights.regular,
-      fontSize: fontSizes.xLarge,
-      fontFamily: defaultFontFamily,
-    },
-
-    h5: {
-      fontStyle: 'normal',
-      fontWeight: weights.regular,
-      fontSize: fontSizes.normal,
-      fontFamily: defaultFontFamily,
-    },
-
-    h6: {
-      fontStyle: 'normal',
-      fontWeight: weights.bolder,
-      fontSize: fontSizes.small,
-      fontFamily: defaultFontFamily,
-    },
-
-    h7: {
-      fontStyle: 'normal',
-      fontWeight: weights.regular,
-      fontSize: fontSizes.xSmall,
-      fontFamily: defaultFontFamily,
-    },
-  },
   fontSizes,
   weights,
   fontFamily: defaultFontFamily,
 };
+
+export default typography;
