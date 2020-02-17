a6481e2af298451f69f4fd2cdfc68c4d2925dd99
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2020-02-17 10:08:36 +0200

    introduce useTheme hook for typing, introduce button cases

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -1,6 +1,7 @@
-import theme from 'theme';
 import { colorPickerBasedOnType, backgroundPickerBasedOnType } from 'utils/themeFunctions';
 import { Props } from 'src/components/Button/Button';
+import { RequiredProperties } from 'src/utils/common';
+import { Theme } from 'src/theme';
 
 /** Calculates the button specific height based on the size passed to it
  * These sizes are specific to this button thus these are placed here and not in the config **/
@@ -15,7 +16,9 @@ const heightBasedOnSize = (size: 'lg' | 'md' | 'sm') => {
   }
 };
 
-export const buttonStyle = ({ type, filled, size }: Props) => ({
+export const buttonStyle = ({ type, filled, size, icon }: RequiredProperties<Props>) => (
+  theme: Theme
+) => ({
   fontSize: theme.typography.fontSizes['16'],
   color: colorPickerBasedOnType(type),
   backgroundColor: filled ? backgroundPickerBasedOnType(type) : 'transparent',
@@ -24,4 +27,6 @@ export const buttonStyle = ({ type, filled, size }: Props) => ({
   height: heightBasedOnSize(size),
   borderRadius: theme.spacing.xsm,
   border: filled ? 'none' : `solid 1px ${theme.palette.gray100}`,
+  display: icon ? 'flex' : 'block',
+  // flexDirection: icon ? 'row' : 'column',
 });
