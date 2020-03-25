7e03c521094165c300789c7753a6df2c6ec14ae1
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2020-03-26 00:45:09 +0200

    Some progress with Radio element

---
+++ src/components/Radio/Radio.style.ts
@@ -0,0 +1,70 @@
+import { css } from '@emotion/core';
+import { Props } from './Radio';
+
+export const inputStyles = css`
+  top: 0;
+  left: 0;
+  width: 100%;
+  cursor: inherit;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  z-index: 1;
+  position: absolute;
+  opacity: 0;
+`;
+
+export const customRadioStyles = (props: Props) => {
+  function determineBoxShadow({ checked, disabled }: Props) {
+    if (disabled) {
+      return 'inset 0px 0px 0px 14px #efefef';
+    }
+
+    if (checked) {
+      return 'inset 0px 0px 0px 2px currentColor, inset 0px 0px 0px 7px #fff, inset 0px 0px 0px 14px currentColor';
+    }
+
+    return 'inset 0px 0px 0px 14px #dfdfdf';
+  }
+
+  return css`
+    transition: all 0.2s ease;
+    border-radius: 50%;
+    width: 28px;
+    height: 28px;
+    box-sizing: border-box;
+    box-shadow: ${determineBoxShadow(props)};
+  `;
+};
+
+export const wrapperStyles = css`
+  position: relative;
+
+  border-radius: 50%;
+
+  width: 50px;
+  height: 50px;
+
+  color: inherit;
+  border: 0;
+  cursor: pointer;
+  margin: 0;
+  display: inline-flex;
+  outline: 0;
+  align-items: center;
+  user-select: none;
+  vertical-align: middle;
+  -moz-appearance: none;
+  justify-content: center;
+  text-decoration: none;
+  -webkit-appearance: none;
+  -webkit-tap-highlight-color: transparent;
+
+  transition: background-color 0.2s ease;
+
+  background-color: transparent;
+
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.04);
+  }
+`;
--- src/components/Radio/Radio.tsx
+++ src/components/Radio/Radio.tsx
@@ -1,18 +1,66 @@
-import React, { ReactEventHandler } from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import { ReactEventHandler, SyntheticEvent, useState } from 'react';
 // import PropTypes from 'prop-types';
+import { customRadioStyles, inputStyles, wrapperStyles } from './Radio.style';
 
 export type Props = {
+  checked: boolean;
   onChange: ReactEventHandler;
+  value: string | number;
+  name?: string;
+  disabled?: boolean;
+  id?: string;
+  required?: boolean;
 };
 
 function Radio(props: Props) {
+  const {
+    onChange = () => {},
+    name = 'yolo',
+    value,
+    checked = false,
+    disabled,
+    id,
+    required,
+  } = props;
+
   return (
-    <div>
-      <input onChange={props.onChange} type={'radio'} />
-    </div>
+    <span css={wrapperStyles}>
+      <input
+        type={'radio'}
+        onChange={onChange}
+        css={inputStyles}
+        name={name}
+        value={value}
+        disabled={disabled}
+        id={id}
+        required={required}
+        checked={checked}
+      />
+      <span css={customRadioStyles(props)} />
+    </span>
   );
 }
 
 Radio.propTypes = {};
 
-export default Radio;
+// export default Radio;
+
+export default function Group() {
+  const [selectedValue, setSelectedValue] = useState('a');
+
+  const handleChange = (event: SyntheticEvent<HTMLInputElement>) => {
+    setSelectedValue((event.target as HTMLInputElement).value);
+  };
+
+  return (
+    <div>
+      <Radio checked={selectedValue === 'a'} value={'a'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'b'} value={'b'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'c'} value={'c'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'd'} value={'d'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'e'} value={'e'} onChange={handleChange} disabled />
+    </div>
+  );
+}
