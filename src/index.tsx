5885fb4b7537d0a647ad5623a6fcab7dcace97c5
christos.zacharopoulos <christos.zacharopoulos@orfium.com>
2020-03-30 12:56:34 +0300

    Expose Icon

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,5 @@ export { default as Select } from './components/Select';
 export { default as Table } from './components/Table';
 export { default as TextField } from './components/TextField';
 export { default as ThemeProvider } from './components/ThemeProvider';
+export { default as Icon } from './components/Icon';
 export { default as useTheme } from './hooks/useTheme';
