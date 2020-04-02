ea2ccacabb588c61d1828a22e24d4c8e266e94b5
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2020-04-02 10:45:02 +0300

    fix svg typing for ReactComponent

--- globals.d.ts
+++ globals.d.ts
@@ -1,6 +1,7 @@
 declare module '*.md';
 
 declare module '*.svg' {
-  const content: string;
-  export default content;
+  const ReactComponent: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+
+  export { ReactComponent };
 }
