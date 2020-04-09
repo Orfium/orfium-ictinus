be85c95091af290fef408f8110409e75af519fa2
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2020-04-09 12:13:18 +0300

    Add RadioButtonsShowcase.tsx component

---
+++ src/components/storyUtils/RadioButtonsShowcase/RadioButtonsShowcase.tsx
@@ -0,0 +1,22 @@
+import Radio from 'components/Radio';
+import React, { ReactEventHandler, useState } from 'react';
+
+function RadioButtonsShowcase() {
+  const [selectedValue, setSelectedValue] = useState('b');
+
+  const handleChange: ReactEventHandler = event => {
+    setSelectedValue((event.target as HTMLInputElement).value);
+  };
+
+  return (
+    <div>
+      <Radio checked={selectedValue === 'a'} value={'a'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'b'} value={'b'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'c'} value={'c'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'd'} value={'d'} onChange={handleChange} />
+      <Radio checked={selectedValue === 'e'} value={'e'} onChange={handleChange} disabled />
+    </div>
+  );
+}
+
+export default RadioButtonsShowcase;
