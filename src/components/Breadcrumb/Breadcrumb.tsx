2d005eca3f19c463fb54ab2b2910ec952b04943d
Michalis Koutridis <michalis.koutridis@orfium.com>
2020-06-09 18:26:58 +0300

    [feature/breadcrumb]: refactors breadcrumb component to receive children components

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 
-interface Props {
+type Props = {
   label: string;
-}
+};
 
-const Breadcrumb: React.FC<Props> = ({ label, children }) => {
+const Breadcrumb: React.FC<Props> = props => {
+  const { children } = props;
+  const getBreadcrumbItem = (child: any, index: number): JSX.Element => {
+    const WrapperComp = child.props.originalType;
+    const innerText = child.props.children;
+    const isLastItem = index === React.Children.toArray(children).length - 1;
+    const separator: JSX.Element | boolean = !isLastItem && (
+      <span style={{ margin: 'auto 4px' }}> / </span>
+    );
+
+    return (
+      <>
+        <WrapperComp key={`${index}_brd`}>{innerText}</WrapperComp>
+        {separator}
+      </>
+    );
+  };
+
+  const breadcrumb: JSX.Element[] = React.Children.toArray(children).map(getBreadcrumbItem);
+
   return (
     <div>
-      <span>{label}</span>
-      {children}
+      <span style={{ display: 'flex', flexDirection: 'row' }}>{breadcrumb}</span>
     </div>
   );
 };
