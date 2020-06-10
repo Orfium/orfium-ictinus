b78500382b4e2b1f308a03f0e3e335ea11c4eb5f
Michalis Koutridis <michalis.koutridis@orfium.com>
2020-06-10 14:34:32 +0300

    [feature/breadcrumb]: adds paading to breadcrumb

--- src/components/Breadcrumb/Breadcrumb.style.ts
+++ src/components/Breadcrumb/Breadcrumb.style.ts
@@ -1,8 +1,10 @@
 import { Theme } from 'theme';
+import { rem } from 'polished';
 import { css } from '@emotion/core';
 
 export const breadcrumbStyles = () => (theme: Theme) => css`
   display: flex;
   flex-wrap: nowrap;
   list-style: none;
+  padding: ${rem(28)} 0;
 `;
