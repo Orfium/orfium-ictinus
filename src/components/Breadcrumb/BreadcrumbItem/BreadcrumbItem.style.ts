5ea95a2b0d845c03cd4c6da6b5098c1f2ca7e836
Michalis Koutridis <michalis.koutridis@orfium.com>
2020-06-10 15:36:22 +0300

    [feature/breadcrumb]: improve styles in Breadcrumb item

--- src/components/Breadcrumb/BreadcrumbItem/BreadcrumbItem.style.ts
+++ src/components/Breadcrumb/BreadcrumbItem/BreadcrumbItem.style.ts
@@ -10,5 +10,9 @@ export const breadcrumbItemStyles = ({ active }: RequiredProperties<StyleProps>)
   theme: Theme
 ) => css`
   display: flex;
+  cursor: pointer;
   color: ${active ? theme.palette.gray300 : theme.palette.gray100};
+  &:hover {
+    color: ${theme.palette.gray300};
+  }
 `;
