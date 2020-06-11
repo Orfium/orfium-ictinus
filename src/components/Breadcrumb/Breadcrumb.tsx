63e03a2faf71f9393b9646cd55ed572efd84a714
Michalis Koutridis <michalis.koutridis@orfium.com>
2020-06-11 18:33:36 +0300

    [feature/breadcrumb]: introduces collapsed items and refactors breadcrumb comp

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,12 +1,13 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { breadcrumbStyles, breadcrumbLinkStyles } from './Breadcrumb.style';
 import useTheme from 'hooks/useTheme';
 import BreadcrumbItem from './BreadcrumbItem/BreadcrumbItem';
 import isEmpty from 'lodash/isEmpty';
 import uniqueId from 'lodash/uniqueId';
 import { Link } from 'react-router-dom';
+import BreadcrumbCollapsed from './BreadcrumbCollapsed/BreadcrumbCollapsed';
 
 export type Props = {
   separatorContent?: '*' | '>' | '/';
@@ -19,15 +20,38 @@ export type BreadcrumbItemData = {
   label: string;
 };
 
+type BreadcrumbItem = React.ReactNode | BreadcrumbItemData;
+
 const Breadcrumb: React.FC<Props> = props => {
   const { children, data = [], separatorContent = '>', breadcrumbItemClickHandler } = props;
   const theme = useTheme();
-  const childrenCollection = React.Children.toArray(children);
-  const isLastItem = (itemIndex: number) =>
-    itemIndex === childrenCollection.length - 1 || itemIndex === data.length - 1;
+  const passDataToRouterLink = ({ to, label }: BreadcrumbItemData) => (
+    <Link css={breadcrumbLinkStyles()(theme)} key={to} to={to}>
+      {label}
+    </Link>
+  );
 
-  const getBreadcrumbItem = (child: React.ReactNode | BreadcrumbItemData, index: number) => {
+  const childrenCollection = React.Children.toArray(children);
+  const dataItems = isEmpty(data) ? childrenCollection : data.map(passDataToRouterLink);
+
+  const isLastItem = (itemIndex: number) => itemIndex === dataItems.length - 1;
+  const shouldCollapse = (item: BreadcrumbItem, itemIndex: number) =>
+    item && dataItems.length > 4 && itemIndex > 0 && itemIndex < dataItems.length - 2;
+
+  const collapsedItems = useCallback(() => dataItems.filter(shouldCollapse), [dataItems]);
+
+  const getBreadcrumbItem = (child: BreadcrumbItem, index: number) => {
     const itemKey = uniqueId('data_item_');
+
+    if (shouldCollapse(child, index)) {
+      return index === 1 ? (
+        <BreadcrumbCollapsed
+          collapsedItems={collapsedItems()}
+          key={itemKey}
+          separatorContent={separatorContent}
+        />
+      ) : null;
+    }
 
     return (
       <BreadcrumbItem
@@ -40,19 +64,9 @@ const Breadcrumb: React.FC<Props> = props => {
     );
   };
 
-  const enhanceIncomingDataWithLink = ({ to, label }: BreadcrumbItemData) => (
-    <Link css={breadcrumbLinkStyles()(theme)} key={to} to={to}>
-      {label}
-    </Link>
-  );
-
-  const breadcrumbItems: JSX.Element[] = isEmpty(data)
-    ? childrenCollection.map(getBreadcrumbItem)
-    : data.map(enhanceIncomingDataWithLink).map(getBreadcrumbItem);
-
   return (
     <ol aria-label="Breadcrumb" css={breadcrumbStyles()(theme)}>
-      {breadcrumbItems}
+      {dataItems.map(getBreadcrumbItem)}
     </ol>
   );
 };
