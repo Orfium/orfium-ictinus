26abfda822c1e5afdd9aee0d25a199e49c4949f4
Michalis Koutridis <michalis.koutridis@orfium.com>
2020-06-15 18:05:26 +0300

    [feature/breadcrumb]: improves breadcrumb collaped styles

--- src/components/Breadcrumb/BreadcrumbCollapsed/BreadcrumbCollapsed.style.ts
+++ src/components/Breadcrumb/BreadcrumbCollapsed/BreadcrumbCollapsed.style.ts
@@ -25,15 +25,18 @@ export const collapsedItemStyles = () => (theme: Theme) => css`
     transition: 0.5s all;
     background: ${theme.palette.gray};
   }
-  height: ${rem(16)};
   font-size: ${theme.typography.fontSizes['14']};
   list-style-type: none;
-  text-align: left;
-  padding: ${rem(16)};
+  & > a {
+    padding: ${rem(16)};
+    display: block;
+    text-align: left;
+  }
 `;
 
 export const inlineBreadcrumbWrapperStyles = {
   padding: 0,
   boxShadow: `#8080802b 0px 0px ${rem(16)}`,
-  top: `${rem(32)}`,
+  top: rem(32),
+  width: rem(164),
 };
