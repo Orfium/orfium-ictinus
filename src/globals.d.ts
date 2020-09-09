df018c32a98d5ceb815127a0f37642fa4c2f570d
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2020-09-09 23:19:09 +0300

    Use one globals.d.ts file

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,10 @@
+declare module '*.md';
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
 type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]> } : T;
 
 type TestId = string;
