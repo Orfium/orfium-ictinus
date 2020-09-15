54978621cfa9f1f9052fcc620664afef0bfe9aa6
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2020-09-16 00:36:13 +0300

    Add script to copy svg assets after build

---
+++ scripts/copy-assets.js
@@ -0,0 +1,24 @@
+/*eslint-disable*/
+const fs = require('fs');
+const path = require('path');
+
+function copyFolderSync(from, to) {
+  if (!fs.existsSync(to)) {
+    fs.mkdirSync(to);
+  }
+  fs.readdirSync(from).forEach(element => {
+    console.log(element);
+    if (fs.lstatSync(path.join(from, element)).isFile()) {
+      if (path.extname(element) === '.svg') {
+        fs.copyFileSync(path.join(from, element), path.join(to, element));
+      }
+    } else {
+      copyFolderSync(path.join(from, element), path.join(to, element));
+    }
+  });
+}
+
+copyFolderSync(
+  path.resolve(__dirname, '../src/components/Icon/assets'),
+  path.resolve(__dirname, '../dist/components/Icon/assets')
+);
