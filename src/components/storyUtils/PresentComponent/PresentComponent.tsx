dbe3efd3dcf7cc2866aec0195f5685d07e4a46de
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2020-09-24 10:41:18 +0300

    fix: stories of icon

--- src/components/storyUtils/PresentComponent/PresentComponent.tsx
+++ src/components/storyUtils/PresentComponent/PresentComponent.tsx
@@ -4,9 +4,10 @@ import * as React from 'react';
 
 type Props = {
   name: string;
+  width?: string;
 };
 
-const PresentComponent: React.FC<Props> = ({ name, children }) => {
+const PresentComponent: React.FC<Props> = ({ name, width = 'auto', children }) => {
   return (
     <div
       style={{
@@ -15,6 +16,7 @@ const PresentComponent: React.FC<Props> = ({ name, children }) => {
         padding: '1rem',
         justifyContent: 'center',
         alignItems: 'center',
+        width,
       }}
     >
       {children}
