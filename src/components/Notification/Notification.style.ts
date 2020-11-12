a88c01dcdc07d13852bb492b55248e1d3519f4be
Konstantinos Danochristos <konstantinos.danochristos@gmail.com>
2020-11-12 16:59:35 +0200

    feat: added containers and more styles

--- src/components/Notification/Notification.style.ts
+++ src/components/Notification/Notification.style.ts
@@ -1,17 +1,57 @@
 import { Theme } from '../../theme';
 import { rem } from 'polished';
 import { NotificationTypes } from './Notification';
+import { css, SerializedStyles } from '@emotion/core';
 
-export const notificationsContainer = (type?: NotificationTypes) => (theme: Theme) => ({
-  width: rem(330),
-  height: rem(58),
-  borderLeft:
-    type === 'success'
-      ? `${theme.palette.success['400']} 4px solid`
+export const notificationsContainer = (type?: NotificationTypes) => (
+  theme: Theme
+): SerializedStyles => css`
+  // display: 'flex';
+  display: grid;
+  align-items: center;
+  justify-content: space-between;
+  width: ${rem(315)};
+  height: ${rem(56)};
+  border-left: ${type === 'success'
+      ? theme.palette.success['400']
       : type === 'error'
-      ? `${theme.palette.error['400']} 4px solid`
+      ? theme.palette.error['400']
       : type === 'info'
-      ? `${theme.palette.info['400']} 4px solid`
-      : `${theme.palette.warning['400']} 4px solid`,
-  borderRadius: rem(4),
-});
+      ? theme.palette.info['400']
+      : theme.palette.warning['400']}
+    4px solid;
+  border-radius: ${rem(4)};
+  border-right: 1px solid black;
+`;
+
+export const infoContainer = () => (): SerializedStyles => css`
+  grid-column-start: 1;
+  display: flex;
+  margin-left: ${rem(16)};
+`;
+
+export const infoMessageContainer = () => (theme: Theme): SerializedStyles => css`
+  // font-size: ${theme.typography.fontSizes['14']};
+`;
+
+export const actionsContainer = () => (): SerializedStyles => css`
+  display: flex;
+  grid-column-start: 2;
+
+  margin-right: ${rem(16)};
+`;
+
+export const primaryActionContainer = () => (theme: Theme): SerializedStyles => css`
+  cursor: pointer;
+  color: rgb(24, 174, 210);
+`;
+
+export const closeIconContainer = () => (): SerializedStyles => css`
+  cursor: pointer;
+  margin-left: ${rem(21)};
+  /* color: rgb(155, 155, 155); */
+`;
+
+export const infoIconContainer = () => (): SerializedStyles => css`
+  margin-right: ${rem(10)};
+`;
--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,15 +1,24 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
 import * as React from 'react';
-import { notificationsContainer } from './Notification.style';
+import {
+  actionsContainer,
+  infoContainer,
+  notificationsContainer,
+  infoIconContainer,
+  infoMessageContainer,
+  primaryActionContainer,
+  closeIconContainer,
+} from './Notification.style';
+import Icon from '../Icon';
 
-export type NotificationTypes = 'success' | 'error' | 'info' | 'warning';
+export type NotificationTypes = 'success' | 'error' | 'info' | 'alert';
 
 export type NotificationVariants = 'inline' | 'banner' | 'toast' | 'modal';
 
 export type Props = {
   /** The (message) informative message of the Notification */
-  message: string[];
+  message: string;
   /** The variant of the Notification */
   variant: NotificationVariants;
   /** The type of the Notification */
@@ -17,9 +26,9 @@ export type Props = {
   /** The primary action of the Notification */
   primaryAction: () => void;
   /** The title (message heading) of the Notification */
-  title?: string[];
+  title?: string;
   /** The description of the Notification */
-  description?: string[];
+  description?: string;
   /** The scondary action of the Notification */
   secondaryAction?: () => void;
 };
@@ -33,9 +42,38 @@ const Notification: React.FC<Props> = ({
   description,
   secondaryAction,
 }) => {
+  const iconColor =
+    type === 'success'
+      ? 'success'
+      : type === 'error'
+      ? 'error'
+      : type === 'info'
+      ? 'info'
+      : type === 'alert'
+      ? 'warning'
+      : 'primary';
+
   return (
     <div css={notificationsContainer(type)}>
-      <p>{message} </p>
+      <div css={infoContainer()}>
+        <div css={infoIconContainer()}>
+          <Icon name={type} color={iconColor} />
+        </div>
+        <div css={infoMessageContainer()}>{message}</div>
+      </div>
+      <div css={actionsContainer()}>
+        <span
+          css={primaryActionContainer()}
+          onClick={e => {
+            e.preventDefault();
+          }}
+        >
+          Action
+        </span>
+        <div css={closeIconContainer()}>
+          <Icon name="close" color="dark" />
+        </div>
+      </div>
     </div>
   );
 };
