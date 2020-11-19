652a1ebf91442c95d7fa18400bdae23fb461d79c
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2020-11-19 18:40:41 +0200

    feat(theme) new function to return colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,4 @@
-import { Palette } from './palette';
+import { colorShades, flatColors, Palette } from './palette';
 import { darkPaletteConfig, lightPaletteConfig, PaletteConfig } from './palette.config';
 import typography, { Typography } from './typography';
 import spacing, { Spacing } from './spacing';
@@ -16,17 +16,29 @@ export type Theme = {
   typography: Typography;
   spacing: Spacing;
   isDark: boolean;
+  getColor: (color: typeof flatColors[number], variant: typeof colorShades[number]) => string;
 };
 
-const defaultTheme = (theming: 'dark' | 'light'): Theme => ({
-  palette:
+const defaultTheme = (theming: 'dark' | 'light'): Theme => {
+  const palette =
     theming === 'light'
       ? enhancePaletteWithShades(lightPaletteConfig)
-      : enhancePaletteWithShades(darkPaletteConfig),
-  typography,
-  spacing,
-  isDark: false,
-});
+      : enhancePaletteWithShades(darkPaletteConfig);
+
+  return {
+    palette,
+    typography,
+    spacing,
+    isDark: false,
+    getColor: (color, variant) => {
+      if (!palette.flat[color][variant]) {
+        throw new Error('No color found with that name');
+      }
+
+      return palette.flat[color][variant];
+    },
+  };
+};
 
 /* Declare any static variables here e.g. $gray: #888; */
 export default defaultTheme;
