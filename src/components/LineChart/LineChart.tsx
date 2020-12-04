5c6e4948def5c23c5c1bf3c1a2a043d412dff939
sotiris <soter.1995@gmail.com>
2020-12-04 11:38:48 +0200

    feature: PreviewChart and GradientLine components created, color function added

--- src/components/LineChart/LineChart.tsx
+++ src/components/LineChart/LineChart.tsx
@@ -3,9 +3,20 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useMemo } from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  AreaChart,
+  Area,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 import { omit, keys, flatten, uniq, sampleSize } from 'lodash';
 import useTheme from '../../hooks/useTheme';
+import GradientLine from './components/GradientLine';
 
 type Data = {
   name: string;
@@ -17,32 +28,34 @@ type Props = {
   labelX?: string;
   labelY?: string;
   showTitles?: boolean;
+  color?: (dataLabel: string) => string;
 };
 
-const Chart = ({ data = [], labelX, labelY, showTitles = false }: Props) => {
+const Chart = ({ data = [], labelX, labelY, showTitles = false, color }: Props) => {
   const theme = useTheme();
 
   const uniqueKeyNames = useMemo(() => {
-    return uniq(
-      data.reduce((acc, curr) => {
-        return [...acc, ...keys(omit(curr, 'name'))];
-      }, [])
-    );
+    return uniq(flatten(data.map(object => keys(omit(object, 'name')))));
+
+    // return uniq(
+    //   data.reduce((acc, curr) => {
+    //     return [...acc, ...keys(omit(curr, 'name'))];
+    //   }, [])
+    // );
   }, [data]);
-  
+
   const colorPicker = useMemo(() => {
-    // const uniqueKeyNames = uniq(flatten(data.map(object => keys(omit(object, 'name')))));
-    // console.log(uniqueKeyNames);
-    // console.log(uniqueKeyNames.length);
-    // console.log(theme.palette);
     const colorSample = sampleSize(theme.palette.flat, uniqueKeyNames.length);
 
     return uniqueKeyNames.reduce((acc, key, index) => {
-      acc[key] = colorSample[index]?.[400];
+      const definedColor = color && color(key) ? color(key) : colorSample[index]?.[400];
+      acc[key] = definedColor;
 
       return acc;
     }, {});
-  }, [uniqueKeyNames, theme]);
+  }, [uniqueKeyNames, theme, color]);
+
+  const colors = Object.entries(colorPicker);
 
   return (
     <>
@@ -54,26 +67,41 @@ const Chart = ({ data = [], labelX, labelY, showTitles = false }: Props) => {
           margin={{ top: 10, right: 30, left: 30, bottom: 10 }}
         >
           <defs>
-            {Object.entries(colorPicker).map(([key, color]) => (
-              <linearGradient key={`color${key}`} id={`color${key}`} x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor={`${color}`} stopOpacity={0.8} />
-                <stop offset="95%" stopColor={`${color}`} stopOpacity={0} />
-              </linearGradient>
+            {colors.map(([dataLabel, color]) => (
+              <GradientLine key={`color${dataLabel}`} label={dataLabel} color={color} />
             ))}
           </defs>
-          <XAxis dataKey="name" label={labelX && { value: labelX, angle: 0, position: 'bottom' }} />
-          <YAxis label={labelY && { value: labelY, angle: -90, position: 'insideLeft' }} />
-          <CartesianGrid strokeDasharray="3 3" />
-          {showTitles && <Legend align="left" iconType="circle" />}
+          <XAxis
+            dataKey="name"
+            tickMargin={5}
+            axisLine={false}
+            tickLine={false}
+            label={labelX && { value: labelX, angle: 0, position: 'bottom' }}
+          />
+          <YAxis
+            axisLine={false}
+            tickLine={false}
+            label={labelY && { value: labelY, angle: -90, position: 'insideLeft' }}
+          />
+          <CartesianGrid vertical={false} />
+          {showTitles && (
+            <Legend
+              align="left"
+              iconType="circle"
+              wrapperStyle={{
+                paddingTop: '0px',
+              }}
+            />
+          )}
           <Tooltip />
-          {Object.entries(colorPicker).map(([key, color]) => (
+          {colors.map(([dataLabel, color]) => (
             <Area
-              key={`${key}`}
+              key={dataLabel}
               type="linear"
-              dataKey={`${key}`}
+              dataKey={`${dataLabel}`}
               stroke={`${color}`}
               fillOpacity={1}
-              fill={`url(#color${key})`}
+              fill={`url(#color${dataLabel})`}
             />
           ))}
         </AreaChart>
---
+++ src/components/LineChart/PreviewChart.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import LineChart from './LineChart';
+
+const initData = [
+  {
+    name: 'Aug 1',
+    hidden: 200,
+    claimed: 40,
+    amt: 240,
+  },
+  {
+    name: 'Aug 2',
+    hidden: 120,
+    claimed: 20,
+    amt: 210,
+  },
+  {
+    name: 'Aug 3',
+    hidden: 79,
+    claimed: 38,
+  },
+  {
+    name: 'Aug 4',
+    hidden: 167,
+    claimed: 22,
+    amt: 20,
+  },
+  {
+    name: 'Aug 5',
+    hidden: 179,
+    claimed: 33,
+    amt: 81,
+  },
+  {
+    name: 'Aug 6',
+    hidden: 61,
+    claimed: 5,
+    amt: 250,
+  },
+  {
+    name: 'Aug 7',
+    hidden: 211,
+    claimed: 45,
+    amt: 100,
+  },
+];
+
+type Data = {
+  name: string;
+  [prop: string]: number | string ;
+};
+
+const PreviewChart = () => {
+  const [data, setData] = React.useState<Data[]>(initData);
+
+
+  const color = (dataLabel:string) => {
+    if (dataLabel === 'hidden') {
+      return 'green'
+    }
+
+    return ''
+  }
+
+  return <LineChart data={data} labelX="Dates" labelY="Values" showTitles={true} color={color} />;
+};
+
+export default PreviewChart;
