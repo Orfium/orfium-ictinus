6a2f49301474c1feb087d721c2feb8a491a17878
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2020-12-15 17:52:18 +0200

    chore(button): add explanation to the new hook function

--- src/hooks/useTypeColorToColorMatch.tsx
+++ src/hooks/useTypeColorToColorMatch.tsx
@@ -21,27 +21,37 @@ export type TypesShadeAndColors = Record<typeof mainTypes[number], ColorShapeFro
 
 /**
  * Apply for each type passed what color it is and shade (shade now is auto applied at 400)
+ * as this is heavy this runs only once at the initialization of the app under the theme provider
  * return e.g { primary: { shade: 400, color: 'orange'}, info: { shade: 400, color: 'darkBlue'}}
  */
 const calculateTypesShadeAndColors = (
   types: Record<typeof mainTypes[number], generatedColorShades>,
   palette: flatPalette
-) =>
-  keys(types).reduce((typeAcc, key) => {
-    const typeColor = types[key][400]; // base color of the type
+) => {
+  // for each mainType
+  return mainTypes.reduce((mainTypeAcc, mainType) => {
+    // save the base color of the the type
+    const typeColor = types[mainType][400]; // base color of the type
+    const flatPaletteKeys = keys(palette); // the keys of the flat palette so we can iterate
 
-    typeAcc[key] = keys(palette).reduce((acc, color) => {
-      const foundShadeWithThatColor = keys(palette[color]).find(shade => {
-        return palette[color][shade] === typeColor;
-      }) as string;
+    // construct a new key in the main object with name from the main type currently in iteration
+    // and return the color found in the flat colors e.g { shade: 400, color: 'orange'}
+    // if nothing found then it will return empty object {}
+    mainTypeAcc[mainType] = flatPaletteKeys.reduce((acc, paletteColor) => {
+      const colorShadesKeys = keys(palette[paletteColor]); // the shades of the palette color currently in the iteration
+      const foundShadeWithThatColor = colorShadesKeys.find(
+        shade => palette[paletteColor][shade] === typeColor
+      );
 
+      // return either the found color as e.g { shade: 400, color: 'orange'} or the object as it was
       return foundShadeWithThatColor
-        ? { shade: Number(foundShadeWithThatColor), color: color }
+        ? { shade: Number(foundShadeWithThatColor), color: paletteColor }
         : acc;
     }, {});
 
-    return typeAcc;
+    return mainTypeAcc;
   }, {}) as TypesShadeAndColors;
+};
 
 const TypeColorToColorMatchProvider: React.FC = ({ children }) => {
   const theme = useTheme();
