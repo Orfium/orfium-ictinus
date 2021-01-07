5bbb5ef18345fda763a2ea4e8dd9d35fbdcffb6d
sotiris <soter.1995@gmail.com>
2021-01-07 16:59:01 +0200

    feat(barchart): PreviewBarChart added

--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -1,31 +1,59 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+} from 'recharts';
 const data = [
-  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+  { name: 'Orfium', value: 600000 },
+  { name: 'UMG', value: 230000 },
+  { name: 'Unknown Owners', value: 138000 },
+  { name: 'SME', value: 60908 },
+  { name: 'AdRev (3d party)', value: 40800 },
+  { name: 'Rock Mobile Corp.', value: 30800 },
+  { name: 'UMPG Publishing', value: 7300 },
+  { name: 'Karya Anak Haki', value: 4300 },
+  { name: 'Adshare (3d party)', value: 3300 },
+  { name: 'Other', value: 14300 },
 ];
+
+// const CustomYAxisTick = (props: { y: number; payload: { value: React.ReactNode; }; }) => {
+//   // console.log(props);
+
+//   return (
+//     <g transform={`translate(${0},${props.y})`}>
+//       <text x={0} y={0} textAnchor="start" fill="#666">
+//         {props.payload.value}
+//       </text>
+//     </g>
+//   );
+// };
+
 const SimpleBarChart = () => {
   return (
-    <BarChart
-      width={600}
-      height={300}
-      data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      layout="vertical"
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis type="number" />
-      <YAxis type="category" dataKey="name" />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="uv" fill="#8884d8" />
-      <Bar dataKey="pv" fill="#82ca9d" />
-    </BarChart>
+    <ResponsiveContainer>
+      <BarChart
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        layout="vertical"
+        barCategoryGap="20%"
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis type="number" />
+        <YAxis type="category" dataKey="name" /*tick={<CustomYAxisTick />}*/ />
+        <Tooltip />
+        <Bar dataKey="value" label={{ position: 'right' }}>
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={index === 0 ? 'orange' : 'gray'} />
+          ))}{' '}
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
   );
 };
 
---
+++ src/components/BarChart/PreviewBarChart.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BarChart from './BarChart';
+
+const PreviewBarChart = () => {
+  return (
+    <div style={{ width: 700, height: 350 }}>
+      <BarChart />
+    </div>
+  );
+};
+
+export default PreviewBarChart;
