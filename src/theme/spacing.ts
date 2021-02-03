2b88f436828d78f9a003ca883a3807701fe96dc8
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2021-02-03 18:44:45 +0200

    style(spacing): change typing on spacing for better readability and usage

--- src/theme/spacing.ts
+++ src/theme/spacing.ts
@@ -1,19 +1,27 @@
 import { rem } from 'polished';
 
+const spaces = {
+  xsm: '4',
+  sm: '8',
+  md: '16',
+  lg: '24',
+  xl: '32',
+} as const;
+
 const spacing: Spacing = {
-  xsm: rem(4),
-  sm: rem(8),
-  md: rem(16),
-  lg: rem(24),
-  xl: rem(32),
+  xsm: rem(Number(spaces.xsm)) as typeof spaces.xsm,
+  sm: rem(Number(spaces.sm)) as typeof spaces.sm,
+  md: rem(Number(spaces.md)) as typeof spaces.md,
+  lg: rem(Number(spaces.lg)) as typeof spaces.lg,
+  xl: rem(Number(spaces.xl)) as typeof spaces.xl,
 };
 
 export type Spacing = {
-  xsm: string;
-  sm: string;
-  md: string;
-  lg: string;
-  xl: string;
+  xsm: typeof spaces.xsm;
+  sm: typeof spaces.sm;
+  md: typeof spaces.md;
+  lg: typeof spaces.lg;
+  xl: typeof spaces.xl;
 };
 
 export default spacing;
