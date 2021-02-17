71f3af5f778afb24de45d915721ef4e8dbcb7dcf
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2021-02-17 11:42:01 +0200

    style(datepicker): fix size of width

--- src/components/utils/TextInputWrapper/TextInputWrapper.style.ts
+++ src/components/utils/TextInputWrapper/TextInputWrapper.style.ts
@@ -104,7 +104,7 @@ export const textFieldStyle = ({ size = DEFAULT_SIZE, label = '', leftIcon, lean
     flex-direction: row;
     align-items: center;
     vertical-align: top;
-    width: calc(100% - ${rem(32)}px);
+    width: calc(100% - ${rem(32)});
     ${!lean && getTextFieldSize(theme, label, Boolean(leftIcon))[size]}
 
     > div {
