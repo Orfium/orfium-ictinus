0c382a858ece2afd3eff60b3dfff8db08a1c5827
Elektra Bilali Simou <ebilali.simou@gmail.com>
2021-02-25 14:58:07 +0200

    chore: added Select tests

--- src/test/utils.tsx
+++ src/test/utils.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
+import userEvent from '@testing-library/user-event';
 import ThemeProvider from '../components/ThemeProvider';
+
+export const selectDropdownOption = async (
+  dropdownInput: HTMLInputElement,
+  option: string
+): Promise<void> => {
+  userEvent.type(dropdownInput, option); // type option on the dropdown input
+  userEvent.click(screen.getByText(option)); // select the option from displayed options
+};
 
 const renderWithThemeProvider = (children: JSX.Element) => {
   return render(
@@ -10,4 +19,5 @@ const renderWithThemeProvider = (children: JSX.Element) => {
 };
 
 export * from '@testing-library/react';
+
 export { renderHook, renderWithThemeProvider as render };
