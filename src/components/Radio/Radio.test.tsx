1ab0344ae1236da3cd01d9ffc33cfc091022d715
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2021-03-05 14:44:25 +0200

    feat(radio): tests

---
+++ src/components/Radio/Radio.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render } from 'test';
+import Radio from './Radio';
+import { fireEvent } from '../../test';
+
+describe('Radio', () => {
+  it('should render correctly', () => {
+    const { container } = render(<Radio />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render correctly with color', () => {
+    const { container } = render(<Radio color={'teal-400'} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should change to checked on click', () => {
+    const mockOnClick = jest.fn();
+    const { getByTestId } = render(<Radio onChange={mockOnClick} dataTestId={'test-check'} />);
+
+    const radio = getByTestId('radio-input-test-check');
+
+    fireEvent.click(radio.children[0]);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(1);
+    expect((radio.children[0] as HTMLInputElement).checked).toBeTruthy();
+  });
+
+  it('disabled should not change to checked on click', () => {
+    const mockOnClick = jest.fn();
+    const { getByTestId } = render(
+      <Radio disabled={true} onChange={mockOnClick} dataTestId={'test-disabled'} />
+    );
+
+    const radio = getByTestId('radio-input-test-disabled');
+
+    fireEvent.click(radio.children[0]);
+
+    expect(mockOnClick).toHaveBeenCalledTimes(0);
+    expect((radio.children[0] as HTMLInputElement).checked).toBeFalsy();
+  });
+});
