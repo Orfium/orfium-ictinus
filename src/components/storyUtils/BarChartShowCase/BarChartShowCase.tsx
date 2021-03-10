e57f6d844b54b85ec535c83f0d3b4351fcef3e92
sotiris <soter.1995@gmail.com>
2021-03-10 17:35:43 +0200

    fix(barchart): change barChartStowCase

--- src/components/storyUtils/BarChartShowCase/BarChartShowCase.tsx
+++ src/components/storyUtils/BarChartShowCase/BarChartShowCase.tsx
@@ -18,6 +18,23 @@ const data: Data[] = [
     },
   },
   { name: 'UMG', value: 230000, barLabel: '15.7%', options: { color: '#c8cade' } },
+  { name: 'Saregama Publishing', value: 138000, barLabel: '8.8%', options: { color: '#c8cade' } },
+  {
+    name: 'PT. Mahar Pustaka Nusantara',
+    value: 60908,
+    barLabel: '7%',
+    options: { color: '#c8cade' },
+  },
+  { name: 'AdRev (3d party)', value: 40800, barLabel: '3.2%', options: { color: '#c8cade' } },
+  { name: 'Rock Mobile Corp.', value: 30800, barLabel: '2.4%', options: { color: '#c8cade' } },
+  {
+    name: 'PT HARMONI DWISELARAS PUBLISHERINDO',
+    value: 7300,
+    barLabel: '1.9%',
+    options: { color: '#c8cade' },
+  },
+  { name: 'Karya Anak Haki', value: 4300, barLabel: '0.7%', options: { color: '#c8cade' } },
+  { name: 'Adshare (3d party)', value: 3300, barLabel: '0.6%', options: { color: '#c8cade' } },
   {
     name: 'Other',
     value: 18300,
