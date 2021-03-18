c4a26c1c6455c30e9dee7590bd3d11a6f78a54c4
Michalis Koutridis <michalis.koutridis@orfium.com>
2021-03-18 18:29:45 +0200

    fix(tooltip): fixes tooltip jsx pragma runtime error

--- src/components/Chart/BarChart/components/CustomTooltipContent/CustomTooltipContent.tsx
+++ src/components/Chart/BarChart/components/CustomTooltipContent/CustomTooltipContent.tsx
@@ -1,5 +1,3 @@
-/** @jsx jsx */
-import { jsx } from '@emotion/core';
 import React from 'react';
 import { tooltipLiStyle, tooltipStyle, tooltipUlStyle } from './CustomTooltipContent.style';
 import { TooltipProps } from 'recharts';
