955bb6a3f2bdfdb2097ba14288f5e27f29283c92
Michalis Koutridis <michalis.koutridis@orfium.com>
2021-04-07 11:08:27 +0300

    fix: adds state update on isChecked param change and adds an demo of this implementation in showcase

--- src/hooks/useCheck.ts
+++ src/hooks/useCheck.ts
@@ -1,9 +1,13 @@
-import { ChangeEvent, useCallback, useState } from 'react';
+import { ChangeEvent, useCallback, useEffect, useState } from 'react';
 
 export type OnCheckHandler = (val: boolean, e: ChangeEvent) => void;
 
 export const useCheck = (isChecked = false, onCheck?: OnCheckHandler) => {
-  const [checked, setChecked] = useState(isChecked);
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(isChecked);
+  }, [isChecked]);
 
   const handleCheck = useCallback(
     (checked, e) => {
