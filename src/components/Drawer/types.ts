9a9cef820e5f70857891dc12447bec10ae449053
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2021-04-12 12:19:32 +0300

    feat(drawer): add optional is active to submenu nav link

--- src/components/Drawer/types.ts
+++ src/components/Drawer/types.ts
@@ -1,4 +1,6 @@
 import { AcceptedIconNames } from 'components/Icon/types';
+import * as H from 'history';
+import { match } from 'react-router';
 
 export type MenuItem = {
   name: string;
@@ -6,5 +8,9 @@ export type MenuItem = {
   state?: Record<string, any> | null;
   visible: boolean;
   iconName: AcceptedIconNames;
+  isActive?<Params extends { [K in keyof Params]?: string }>(
+    match: match<Params> | null,
+    location: H.Location
+  ): boolean;
   options: MenuItem[];
 };
