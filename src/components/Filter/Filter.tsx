e785597b5a2ceb6641d1fa750cd724e5c7651348
Katerina Boutsika <kboutsika@helloworld.gr>
2021-05-12 13:11:26 +0300

    feat: add filter component

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,102 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import * as React from 'react';
+
+import useTheme from 'hooks/useTheme';
+import { useTypeColorToColorMatch } from 'hooks/useTypeColorToColorMatch';
+import Icon from '../Icon';
+import { generateTestDataId } from '../../utils/helpers';
+import ClickAwayListener from '../utils/ClickAwayListener';
+import { optionsStyle } from '../utils/DropdownOptions';
+import { Props, TestProps, } from './types';
+import { getTextColor } from './utils';
+import {
+  buttonStyle,
+  childrenWrapperStyle,
+  buttonSpanStyle,
+  labelSpanStyle,
+  wrapperStyle,
+} from './Filter.style';
+
+const Filter: React.FC<Props & TestProps> = props => {
+  const {
+    items,
+    onSelect,
+    defaultValue,
+    selectedItem,
+    styleType,
+    dataTestId,
+    label = '',
+    color = '',
+    buttonType = 'primary',
+    disabled = false,
+  } = props;
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+  const { calculateColorBetweenColorAndType } = useTypeColorToColorMatch();
+  const hasSelectedValue = defaultValue.value !== selectedItem.value;
+  const activeCalculatedColor = calculateColorBetweenColorAndType('', 'primary');
+  const calculatedColor = calculateColorBetweenColorAndType(color, buttonType);
+  const iconColor = getTextColor({
+    open,
+    theme,
+    activeCalculatedColor,
+    calculatedColor,
+    hasSelectedValue,
+  });
+  const iconName = open ? 'triangleUp' : 'triangleDown';
+
+  return (
+    <ClickAwayListener onClick={() => setOpen(false)}>
+      <div css={wrapperStyle()} data-testid={dataTestId}>
+        <button
+          data-testid={generateTestDataId('filter', dataTestId)}
+          css={buttonStyle({
+            calculatedColor,
+            activeCalculatedColor,
+            buttonType,
+            disabled,
+            open,
+            styleType,
+            hasSelectedValue,
+          })}
+          onClick={() => setOpen(!open)}
+          disabled={disabled}
+        >
+          <span css={buttonSpanStyle()}>
+            <span css={childrenWrapperStyle()}>
+              <span css={labelSpanStyle(open, hasSelectedValue)}>{label}:
+                <span>{selectedItem.label}</span>
+              </span>
+            </span>
+
+            <Icon name={iconName} color={iconColor} size={6} />
+          </span>
+        </button>
+        {open && (
+          <div css={optionsStyle({})(theme)} data-testid="filter-menu">
+            {items &&
+              items.map((option, index) => (
+                <button
+                  css={{
+                    backgroundColor: theme.palette.white,
+                    border: 0,
+                  }}
+                  key={`${option.value}-${index}`}
+                  onClick={() => {
+                    setOpen(false);
+                    onSelect(option);
+                  }}
+                >
+                  {option.label}
+                </button>
+              ))}
+          </div>
+        )}
+      </div>
+    </ClickAwayListener>
+  );
+};
+
+export default Filter;
---
+++ src/components/Filter/types.ts
@@ -0,0 +1,59 @@
+import { AcceptedColorComponentTypes, ColorShapeFromComponent } from '../../utils/themeFunctions';
+import * as React from 'react';
+import { Theme } from '../../theme';
+
+export type FilterOption = {
+  value: string | number;
+  label: string;
+};
+
+export type StyleType = 'filled' | 'outlined' | 'elevated' | 'transparent';
+
+export type Props = {
+  /** the color of the button when the menu is opened based on our colors eg. red-400 */
+  color?: string;
+  /** The type of the button - defaults to "primary" */
+  buttonType?: AcceptedColorComponentTypes;
+  /** Items that are being declared as menu options */
+  items?: FilterOption[];
+  /** The item selected on the menu */
+  selectedItem: FilterOption;
+  /** The default selected item */
+  defaultValue: FilterOption;
+  /** A callback that is being triggered when an items has been clicked */
+  onSelect: (option: FilterOption) => void;
+  /** The text of the button to show */
+  label: React.ReactNode;
+  /** Defines if the button is in disabled state */
+  disabled?: boolean;
+  /** Defines the style type of the button */
+  styleType: StyleType;
+};
+
+export type TestProps = {
+  dataTestId?: string;
+};
+
+
+export type BaseColorProps = {
+  open: boolean;
+  theme: Theme;
+  calculatedColor: ColorShapeFromComponent;
+  activeCalculatedColor: ColorShapeFromComponent;
+  hasSelectedValue: boolean;
+}
+
+export type BackgroundColorProps = BaseColorProps & {
+  buttonType: AcceptedColorComponentTypes;
+  styleType: StyleType;
+};
+
+export type ButtonStyleProps = Omit<BaseColorProps, 'theme'> & {
+  disabled?: boolean;
+  styleType: StyleType;
+  buttonType: AcceptedColorComponentTypes;
+};
+
+export type BorderProps = Omit<BaseColorProps, 'open' | 'calculatedColor'> & {
+  styleType: StyleType;
+};
