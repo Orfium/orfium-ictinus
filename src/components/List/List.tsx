4176743783d1804beb0d8b86d1a4adbfbf7602df
kostasdano <kostas.danochristos@orfium.com>
2021-06-10 19:47:18 +0300

    feat(list): introducing virtualized list

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { List as VList } from 'react-virtualized';
 
 import ListItem from './ListItem';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const items = Array.from(new Array(500), (_, index) => 'Item ' + index);
+
+const rowRenderer = ({ index, key, style }: { index: number; key: string; style: any }) => (
+  <span style={style} key={key}>
+    <ListItem size={'small'} content={items[index]} index={index} rightIcon="earnings" hasCheckbox />
+  </span>
+);
+
 const List: React.FC = () => {
   return (
-    <div style={{ width: '220px' }}>
-      <ListItem size={'normal'} content="list item" />
-    </div>
+    <VList
+      width={300}
+      height={300}
+      rowCount={items.length}
+      rowHeight={46}
+      rowRenderer={rowRenderer}
+    />
   );
 };
 
