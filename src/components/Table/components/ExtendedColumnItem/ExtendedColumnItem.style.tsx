cf7790144925d83081a3634176dc10506ff698a4
George Metakos <george.metakos@orfium.com>
2021-06-22 21:23:04 +0300

    Feat/ds 188 table sorting and tooltips
    Feat/ds 188 table sorting and tooltips

---
+++ src/components/Table/components/ExtendedColumnItem/ExtendedColumnItem.style.tsx
@@ -0,0 +1,16 @@
+import { css, SerializedStyles } from '@emotion/core';
+import { rem } from 'polished';
+
+import { flexCenterVertical } from 'theme/functions';
+import { Theme } from '../../../../theme';
+
+export const containerStyles = (gap: string) => (): SerializedStyles => css`
+  ${flexCenterVertical};
+  display: inline-flex;
+  gap: ${rem(`${gap}px`)};
+`;
+
+export const contentStyles = () => (theme: Theme): SerializedStyles => css`
+  font-weight: ${theme.typography.weights.medium};
+  color: ${theme.utils.getColor('lightGray', 600)};
+`;
