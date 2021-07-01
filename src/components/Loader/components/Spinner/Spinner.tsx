25a03fcd2a49797a0afbd6d3f304a25d1ca7e504
Michalis Koutridis <michalis.koutridis@gmail.com>
2021-07-01 10:47:08 +0300

    refactor: changes after review

--- src/components/Loader/components/Spinner/Spinner.tsx
+++ src/components/Loader/components/Spinner/Spinner.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from 'react';
 import { spinnerContainer } from './Spinner.style';
 import { generateTestDataId } from '../../../../utils/helpers';
+import { TestId } from '../../../../utils/types';
 
 interface Props {
-  dataTestId?: string;
+  dataTestId?: TestId;
 }
 const Spinner: FC<Props> = ({ dataTestId }) => {
   return <div data-testid={generateTestDataId('spinner', dataTestId)} css={spinnerContainer()} />;
