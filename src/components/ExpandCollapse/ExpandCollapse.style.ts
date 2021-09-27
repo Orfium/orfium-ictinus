58915550bbf2a6b10da2eb8eb01ae1aa053d9f31
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2021-09-27 16:32:49 +0300

    feat(styles): change px to rem on ExpandCollapse and update snaps

--- src/components/ExpandCollapse/ExpandCollapse.style.ts
+++ src/components/ExpandCollapse/ExpandCollapse.style.ts
@@ -1,8 +1,9 @@
 import { css } from '@emotion/react';
+import { rem } from 'polished';
 
 export const contentStyles = (expanded: boolean, transitionDuration: number) => css`
   transition: opacity ${expanded ? transitionDuration + 100 : 0}ms ease,
     transform ${expanded ? transitionDuration + 100 : 0}ms ease;
   opacity: ${Number(expanded)};
-  transform: translateY(${expanded ? 0 : '-10px'});
+  transform: translateY(${expanded ? 0 : rem('-10px')});
 `;
