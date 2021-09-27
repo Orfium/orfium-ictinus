e47d238da7a8ee8ccc8f3ff36a731acc456a231d
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2021-09-27 13:41:11 +0300

    feat(styles): change px to rem on Label and update snaps

--- src/components/Label/Label.style.ts
+++ src/components/Label/Label.style.ts
@@ -1,26 +1,21 @@
 import { css, SerializedStyles } from '@emotion/react';
+import { rem } from 'polished';
 
 import { Theme } from '../../theme';
 
-export const LABEL_TRANSFORM_LEFT_SPACING = '3';
+export const LABEL_TRANSFORM_LEFT_SPACING = '3px';
 
-export const labelStyle = ({
-  size,
-  animateToTop,
-  error,
-}: {
-  size: string;
-  animateToTop: boolean;
-  error: boolean;
-}) => (theme: Theme): SerializedStyles => css`
+export const labelStyle = ({ animateToTop, error }: { animateToTop: boolean; error: boolean }) => (
+  theme: Theme
+): SerializedStyles => css`
   transition: transform 0.25s, opacity 0.25s ease-in-out;
   transform-origin: 0 0;
   width: 100%;
   position: absolute;
   user-select: none;
   transform: ${!animateToTop
-    ? `translate(${LABEL_TRANSFORM_LEFT_SPACING}px, 0)`
-    : `translate(${LABEL_TRANSFORM_LEFT_SPACING}px, -95%) scale(0.8);`};
+    ? `translate(${rem(LABEL_TRANSFORM_LEFT_SPACING)}, 0)`
+    : `translate(${rem(LABEL_TRANSFORM_LEFT_SPACING)}, -95%) scale(0.8);`};
   font-size: ${theme.typography.fontSizes['16']};
   font-weight: ${theme.typography.weights.regular};
   color: ${error
