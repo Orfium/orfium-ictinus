6c850f22ccd14b5484a7c5051b8ee31f28a7823e
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2021-09-28 12:01:15 +0300

    feat(styles): revert

--- src/components/Label/Label.style.ts
+++ src/components/Label/Label.style.ts
@@ -5,9 +5,15 @@ import { Theme } from '../../theme';
 
 export const LABEL_TRANSFORM_LEFT_SPACING = '3px';
 
-export const labelStyle = ({ animateToTop, error }: { animateToTop: boolean; error: boolean }) => (
-  theme: Theme
-): SerializedStyles => css`
+export const labelStyle = ({
+  size,
+  animateToTop,
+  error,
+}: {
+  size: string;
+  animateToTop: boolean;
+  error: boolean;
+}) => (theme: Theme): SerializedStyles => css`
   transition: transform 0.25s, opacity 0.25s ease-in-out;
   transform-origin: 0 0;
   width: 100%;
