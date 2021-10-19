e20c415cf5a919e1183b2cfb4b0a4b512a36f943
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2021-10-19 10:47:19 +0300

    chore: minor remove the radius not needed on the list

--- src/components/List/List.style.ts
+++ src/components/List/List.style.ts
@@ -5,6 +5,5 @@ export const wrapperStyle = ({ width }: { width: number | undefined }) => (
   theme: Theme
 ): SerializedStyles => css`
   border: 1px solid ${theme.utils.getColor('lightGrey', 100)};
-  border-radius: ${theme.spacing.xsm};
   width: ${`${width}px` || '100%'};
 `;
