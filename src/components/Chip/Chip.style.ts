dc19fdb09cf5df87280102d3fac4a8781bdc1a8d
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2022-01-21 12:59:46 +0200

    feat(global states): integrate woth chip

--- src/components/Chip/Chip.style.ts
+++ src/components/Chip/Chip.style.ts
@@ -3,6 +3,7 @@ import { flexCenterVertical } from 'theme/functions';
 import { rem } from 'theme/utils';
 
 import { Theme } from '../../theme';
+import { getDisabled, getFocus, getHover, getPressed } from '../../theme/states';
 import { Props } from './Chip';
 
 export const chipStyle = ({
@@ -11,38 +12,63 @@ export const chipStyle = ({
   isSelected,
   onClear,
   onClick,
-}: Props) => (theme: Theme): SerializedStyles => css`
-  ${flexCenterVertical};
-  height: ${theme.spacing.lg};
-  border-radius: ${theme.spacing.lg};
-  font-size: ${theme.typography.fontSizes['12']};
-  font-weight: ${theme.typography.weights.medium};
-  line-height: normal;
-  box-sizing: border-box;
-  padding: ${theme.spacing.xsm} ${theme.spacing.sm};
-  background-color: ${styleType === 'read-only' || onClear || isSelected
-    ? theme.utils.getColor(fill, 50)
-    : theme.palette.white};
-  color: ${theme.utils.getColor('darkGrey', 850)};
-  border: ${rem(1)} solid
-    ${styleType === 'read-only' || onClear || isSelected
-      ? theme.utils.getColor(fill, 550)
-      : theme.utils.getColor('lightGrey', 200)};
-  cursor: ${onClick ? 'pointer' : 'auto'};
-  width: fit-content;
-  transition: background-color 150ms linear;
+}: Props) => (theme: Theme): SerializedStyles => {
+  const customFilled = styleType === 'read-only' || onClear || isSelected;
 
-  &:hover {
-    background: ${styleType === 'read-only' || onClear || isSelected
-      ? theme.utils.getColor(fill, 100)
-      : theme.utils.getColor('lightGrey', null, 'pale')};
-  }
+  return css`
+    ${flexCenterVertical};
+    height: ${theme.spacing.lg};
+    border-radius: ${theme.spacing.lg};
+    font-size: ${theme.typography.fontSizes['12']};
+    font-weight: ${theme.typography.weights.medium};
+    line-height: normal;
+    box-sizing: border-box;
+    padding: ${theme.spacing.xsm} ${theme.spacing.sm};
+    background-color: ${customFilled ? theme.utils.getColor(fill, 50) : theme.palette.white};
+    color: ${theme.utils.getColor('darkGrey', 850)};
+    border: ${rem(1)} solid
+      ${customFilled ? theme.utils.getColor(fill, 550) : theme.utils.getColor('lightGrey', 200)};
+    cursor: ${onClick ? 'pointer' : 'auto'};
+    width: fit-content;
+    transition: background-color 150ms linear;
 
-  > :not(:last-child) {
-    margin-right: ${theme.spacing.xsm};
-  }
-`;
+    &:hover:not(:disabled) {
+      background: ${customFilled
+        ? getHover({ theme, color: fill, shade: 50 }).backgroundColor
+        : getHover({ theme }).backgroundColor};
+    }
 
-export const closeIconWrapperStyle = () => (): SerializedStyles => css`
-  cursor: pointer;
+    &:focus-visible:not(:disabled) {
+      outline: ${styleType === 'read-only'
+        ? 'none'
+        : getFocus({ theme, borderWidth: 1 }).styleOutline};
+    }
+
+    ${styleType !== 'read-only' &&
+      `
+    &:active:not(:disabled) {
+      background: ${
+        getPressed({
+          theme,
+          color: fill,
+          shade: 50,
+        }).backgroundColor
+      };
+    }
+    ,
+    
+      :disabled {
+        opacity: ${getDisabled().opacity};
+        cursor: ${getDisabled().cursor};
+      }
+    `}
+
+    > :not(:last-child) {
+      margin-right: ${theme.spacing.xsm};
+    }
+  `;
+};
+
+export const closeIconWrapperStyle = (disabled?: boolean) => (): SerializedStyles => css`
+  cursor: ${disabled ? getDisabled().cursor : 'pointer'};
 `;
