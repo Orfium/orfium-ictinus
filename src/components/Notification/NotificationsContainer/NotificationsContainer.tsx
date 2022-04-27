16199062accb94458a56ee53937ac79ab52a4ec2
Dimitris Karagiannis <mitch.karajohn@gmail.com>
2022-04-28 01:25:41 +0300

    Update implementation of NotificationsContainer.tsx to be able to be placed within other elements

--- src/components/Notification/NotificationsContainer/NotificationsContainer.tsx
+++ src/components/Notification/NotificationsContainer/NotificationsContainer.tsx
@@ -1,4 +1,6 @@
-import * as React from 'react';
+import { ReactNode } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { notificationsContainer } from './NotificationsContainer.style';
 
@@ -7,10 +9,22 @@ type Positions = 'top-right' | 'top-left' | 'bottom-left' | 'bottom-right';
 type Props = {
   /** Notifications Container position */
   position: Positions;
+  children: ReactNode;
+  parent?: HTMLElement | null;
 };
 
-const NotificationsContainer: React.FC<Props> = ({ children, position }) => {
-  return <div css={notificationsContainer(position)}>{children}</div>;
+const NotificationsContainer: React.FC<Props> = props => {
+  const { children, position, parent = document.body } = props;
+  console.log(parent);
+
+  if (parent === null) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
+    <div css={notificationsContainer(position, parent)}>{children}</div>,
+    parent
+  );
 };
 
 export default NotificationsContainer;
--- src/components/storyUtils/NotificationShowcase/NotificationShowcase.tsx
+++ src/components/storyUtils/NotificationShowcase/NotificationShowcase.tsx
@@ -1,14 +1,47 @@
-import React, { useState } from 'react';
+import { select } from '@storybook/addon-knobs';
+import React, { ReactNode, useRef, useState } from 'react';
 
 import Button from '../../Button';
+import NotificationsContainer from '../../Notification/NotificationsContainer';
 
-const NotificationShowcase: React.FC = ({ children }) => {
+const NotificationShowcase: React.FC<{ id?: string }> = ({ children, id }) => {
   const [show, setShow] = useState<boolean>(false);
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div id={id} style={{ width: '100%', height: '100%' }}>
       <Button onClick={() => setShow(!show)}>Show Notifications</Button>
       {show && children}
+    </div>
+  );
+};
+
+export const NotificationContainerWithinDOMElement = (props: {
+  children: ReactNode;
+  id: string;
+}) => {
+  const { children, id } = props;
+  const [divEl, setDivEl] = useState<HTMLDivElement | null>(null);
+
+  console.log('yolo');
+
+  return (
+    <div
+      ref={el => {
+        setDivEl(el);
+      }}
+      id={id}
+      style={{ width: '928px', height: '500px', position: 'relative' }}
+    >
+      <NotificationsContainer
+        parent={divEl}
+        position={select(
+          'position',
+          ['top-right', 'top-left', 'bottom-left', 'bottom-right'],
+          'bottom-right'
+        )}
+      >
+        {children}
+      </NotificationsContainer>
     </div>
   );
 };
