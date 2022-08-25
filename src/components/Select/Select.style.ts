46c4e9fbc0edf6c87ec8aa6a3acfad132cc60951
Christos-Zacharopoulos <zacharopoulos.chris@gmail.com>
2022-08-25 16:40:04 +0300

    refactor(bool naming): Select.style

--- src/components/Select/Select.style.ts
+++ src/components/Select/Select.style.ts
@@ -2,25 +2,23 @@ import { css, SerializedStyles } from '@emotion/react';
 import { transition } from 'theme/functions';
 import { rem } from 'theme/utils';
 
-export const selectWrapper = ({
-  isSearchable,
-}: {
-  isSearchable: boolean;
-}) => (): SerializedStyles => css`
-  position: relative;
-  min-width: ${rem(150)};
+export const selectWrapper =
+  ({ isSearchable }: { isSearchable: boolean }) =>
+  (): SerializedStyles =>
+    css`
+      position: relative;
+      min-width: ${rem(150)};
 
-  * {
-    cursor: ${!isSearchable && 'pointer'};
-  }
-`;
+      * {
+        cursor: ${!isSearchable && 'pointer'};
+      }
+    `;
 
-export const rightIconContainer = (
-  open: boolean,
-  isSearchable: boolean
-) => (): SerializedStyles => css`
-  display: flex;
-  cursor: pointer;
-  transform: rotate(${open && !isSearchable ? '180' : '0'}deg);
-  ${transition(0.2)}
-`;
+export const rightIconContainer =
+  (isOpen: boolean, isSearchable: boolean) => (): SerializedStyles =>
+    css`
+      display: flex;
+      cursor: pointer;
+      transform: rotate(${isOpen && !isSearchable ? '180' : '0'}deg);
+      ${transition(0.2)}
+    `;
