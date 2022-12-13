de66c84e9bc560d92482920a3f732a89538c831e
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2022-12-13 17:25:27 +0200

    chore: date picker with fixed dates for correct comparison

--- src/components/DatePicker/utils.ts
+++ src/components/DatePicker/utils.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 import { Range } from './OverlayComponent/OverlayComponent';
 
-export const currentDay = process.env.NODE_ENV !== 'test' ? dayjs() : dayjs('11-03-2020 12:00:00');
+export const currentDay = dayjs('11-03-2020 12:00:00');
 
 export const datepickerPropValue =
   process.env.NODE_ENV !== 'test' ? undefined : dayjs('11-03-2020 12:00:00');
