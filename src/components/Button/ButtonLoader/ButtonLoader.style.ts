db07258965a2c64e7b9e3c4ea65164a14d73c063
kostasdano <kostas.danochristos@orfium.com>
2023-01-23 13:07:11 +0200

    feat: button loader

--- src/components/Button/ButtonLoader/ButtonLoader.style.ts
+++ src/components/Button/ButtonLoader/ButtonLoader.style.ts
@@ -1,9 +1,62 @@
-import { css, SerializedStyles } from '@emotion/react';
-import { flex } from 'theme/functions';
+import { css, SerializedStyles, keyframes } from '@emotion/react';
+import { Theme } from 'theme';
 import { rem } from 'theme/utils';
 
-export const centralizedLoader: (clientWidth?: number) => SerializedStyles = clientWidth => css`
-  width: ${clientWidth ? rem(clientWidth) : 'auto'};
-  ${flex};
-  justify-content: center;
-`;
+import stateTokens from '../../../theme/states/states.tokens';
+import buttonTokens from '../../Button/Button.tokens';
+import { ButtonBaseProps } from 'components/ButtonBase';
+
+const getBarHeight = ({ type }: Pick<ButtonBaseProps, 'type'>) =>
+  type === 'invertedAlt' || type === 'tertiary' ? rem(2) : rem(4);
+
+export const loaderWrapperStyle =
+  ({ type = 'primary' }: Pick<ButtonBaseProps, 'type'>) =>
+  (): SerializedStyles => {
+    return css({
+      width: '100%',
+      height: getBarHeight({ type }),
+      position: 'absolute',
+      borderTopLeftRadius: buttonTokens.borderRadius.text,
+      borderTopRightRadius: buttonTokens.borderRadius.text,
+      boxShadow: `0 -2px 0px 0px inset ${stateTokens.active.backgroundColor[type]}`,
+    });
+  };
+
+export const barWrapperStyle =
+  ({ type = 'primary' }: Pick<ButtonBaseProps, 'type'>) =>
+  (theme: Theme): SerializedStyles => {
+    return css({
+      width: '100%',
+      height: getBarHeight({ type }),
+      overflow: 'hidden',
+      backgroundColor: theme.utils.getColor('magenta', 150),
+      borderTopLeftRadius: buttonTokens.borderRadius.text,
+      borderTopRightRadius: buttonTokens.borderRadius.text,
+      boxShadow: `0 -2px 0px 0px inset ${stateTokens.active.backgroundColor[type]}`,
+      position: 'relative',
+    });
+  };
+
+export const barStyle =
+  ({ type = 'primary' }: Pick<ButtonBaseProps, 'type'>) =>
+  (): SerializedStyles => {
+    const barAnimation = keyframes({
+      from: {
+        left: '-50%',
+      },
+      to: {
+        left: '100%',
+      },
+    });
+
+    return css({
+      width: '50%',
+      background: 'linear-gradient(270deg, #F814E1 0.02%, #4945EE 100%)',
+      height: getBarHeight({ type }),
+      boxShadow: `0 -2px 0px 0px inset ${stateTokens.active.backgroundColor[type]}`,
+      position: 'absolute',
+      bottom: '0',
+      top: '0',
+      animation: `${barAnimation} 1.5s infinite`,
+    });
+  };
--- src/components/Button/ButtonLoader/ButtonLoader.tsx
+++ src/components/Button/ButtonLoader/ButtonLoader.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
+import { TestProps } from 'utils/types';
 
-import { centralizedLoader } from './ButtonLoader.style';
-import Loader from 'components/Loader';
+import { barStyle, barWrapperStyle, loaderWrapperStyle } from './ButtonLoader.style';
+import { ButtonBaseProps } from 'components/ButtonBase';
 
-export type ButtonLoaderProps = {
-  innerButtonWidth?: number;
-};
+export type ButtonLoaderProps = Pick<ButtonBaseProps, 'type'> & TestProps;
 
-const ButtonLoader: React.FC<ButtonLoaderProps> = ({ innerButtonWidth }) => {
+const ButtonLoader: React.FC<ButtonLoaderProps> = ({ type, dataTestId }) => {
   return (
-    <div css={centralizedLoader(innerButtonWidth)}>
-      <Loader type={'spinner'} color={'#123456'} />
+    <div css={loaderWrapperStyle({ type })} data-testid={dataTestId}>
+      <div css={barWrapperStyle({ type })}>
+        <div css={barStyle({ type })} />
+      </div>
     </div>
   );
 };
 
-ButtonLoader.displayName = 'Button';
+ButtonLoader.displayName = 'ButtonLoader';
 
 export default ButtonLoader;
