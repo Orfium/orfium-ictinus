16fa2518d67f1a62b0dab562ed7333f00720400c
kostasdano <kostas.danochristos@orfium.com>
2023-02-21 14:46:00 +0200

    feat: update showcase and snapshots

---
+++ src/components/storyUtils/EdgeCasesSelectShowcase/EdgeCasesSelectShowcase.style.ts
@@ -0,0 +1,7 @@
+import { css, SerializedStyles } from '@emotion/react';
+
+export const showcaseContainer = (): SerializedStyles => css`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
