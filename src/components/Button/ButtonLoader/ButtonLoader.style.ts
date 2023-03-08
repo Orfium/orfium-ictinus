d499672b0d84dc8f09f35ade67b420dc3cdf15b8
kostasdano <kostas.danochristos@orfium.com>
2023-03-08 13:54:19 +0200

    refactor: button loader component styles

--- src/components/Button/ButtonLoader/ButtonLoader.style.ts
+++ src/components/Button/ButtonLoader/ButtonLoader.style.ts
@@ -2,72 +2,55 @@ import { css, SerializedStyles, keyframes } from '@emotion/react';
 import { Theme } from 'theme';
 import { rem } from 'theme/utils';
 
-import getStatesTokens from '../../../theme/states/states.tokens';
 import getButtonTokens from '../../Button/Button.tokens';
-import { ButtonTypes } from '../Button.types';
-import { ButtonBaseProps } from 'components/ButtonBase';
 
-const getBarHeight = ({ type }: Pick<ButtonBaseProps, 'type'>) =>
-  type === 'invertedAlt' || type === 'tertiary' ? rem(2) : rem(4);
-
-const getBoxShadowBackgroundColor = (type: ButtonTypes) => (theme: Theme) => {
-  const stateTokens = getStatesTokens(theme);
-
-  return `0 -2px 0px 0px inset ${stateTokens.active.backgroundColor[type]}`;
+export const loaderWrapperStyle = (): SerializedStyles => {
+  return css({
+    width: '100%',
+    position: 'absolute',
+  });
 };
 
-export const loaderWrapperStyle =
-  ({ type = 'primary' }: Pick<ButtonBaseProps, 'type'>) =>
-  (theme: Theme): SerializedStyles => {
-    const buttonTokens = getButtonTokens(theme);
-
-    return css({
-      width: '100%',
-      height: getBarHeight({ type }),
-      position: 'absolute',
-      borderTopLeftRadius: buttonTokens.borderRadius.text,
-      borderTopRightRadius: buttonTokens.borderRadius.text,
-      boxShadow: getBoxShadowBackgroundColor(type)(theme),
-    });
-  };
-
 export const barWrapperStyle =
-  ({ type = 'primary' }: Pick<ButtonBaseProps, 'type'>) =>
+  () =>
   (theme: Theme): SerializedStyles => {
     const buttonTokens = getButtonTokens(theme);
 
     return css({
       width: '100%',
-      height: getBarHeight({ type }),
+      height: rem(4),
       overflow: 'hidden',
-      backgroundColor: theme.utils.getColor('magenta', 150),
       borderTopLeftRadius: buttonTokens.borderRadius.text,
       borderTopRightRadius: buttonTokens.borderRadius.text,
-      boxShadow: getBoxShadowBackgroundColor(type)(theme),
       position: 'relative',
+
+      ':before': {
+        content: '""',
+        width: '100%',
+        height: rem(2),
+        background: theme.utils.getColor('magenta', 150),
+        display: 'block',
+      },
     });
   };
 
-export const barStyle =
-  ({ type = 'primary' }: Pick<ButtonBaseProps, 'type'>) =>
-  (theme: Theme): SerializedStyles => {
-    const barAnimation = keyframes({
-      from: {
-        left: '-50%',
-      },
-      to: {
-        left: '100%',
-      },
-    });
+export const barStyle = (): SerializedStyles => {
+  const barAnimation = keyframes({
+    from: {
+      left: '-50%',
+    },
+    to: {
+      left: '100%',
+    },
+  });
 
-    return css({
-      width: '50%',
-      background: 'linear-gradient(270deg, #F814E1 0.02%, #4945EE 100%)',
-      height: getBarHeight({ type }),
-      boxShadow: getBoxShadowBackgroundColor(type)(theme),
-      position: 'absolute',
-      bottom: '0',
-      top: '0',
-      animation: `${barAnimation} 1.5s infinite`,
-    });
-  };
+  return css({
+    width: '50%',
+    background: 'linear-gradient(270deg, #F814E1 0.02%, #4945EE 100%)',
+    height: rem(2),
+    position: 'absolute',
+    bottom: '0',
+    top: '0',
+    animation: `${barAnimation} 1.5s infinite`,
+  });
+};
--- src/components/Button/ButtonLoader/ButtonLoader.tsx
+++ src/components/Button/ButtonLoader/ButtonLoader.tsx
@@ -2,15 +2,14 @@ import React from 'react';
 import { TestProps } from 'utils/types';
 
 import { barStyle, barWrapperStyle, loaderWrapperStyle } from './ButtonLoader.style';
-import { ButtonBaseProps } from 'components/ButtonBase';
 
-export type ButtonLoaderProps = Pick<ButtonBaseProps, 'type'> & TestProps;
+export type ButtonLoaderProps = TestProps;
 
-const ButtonLoader: React.FC<ButtonLoaderProps> = ({ type, dataTestId }) => {
+const ButtonLoader: React.FC<ButtonLoaderProps> = ({ dataTestId }) => {
   return (
-    <div css={loaderWrapperStyle({ type })} data-testid={dataTestId}>
-      <div css={barWrapperStyle({ type })}>
-        <div css={barStyle({ type })} />
+    <div css={loaderWrapperStyle()} data-testid={dataTestId}>
+      <div css={barWrapperStyle()}>
+        <div css={barStyle()} />
       </div>
     </div>
   );
