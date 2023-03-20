bc18da4ef09cf660c0d4e560e44deb9fc2983ce6
kostasdano <kostas.danochristos@orfium.com>
2023-03-20 13:52:32 +0200

    feat: make toast min-height optional

--- src/components/Toast/Toast.style.ts
+++ src/components/Toast/Toast.style.ts
@@ -22,57 +22,69 @@ const toastContainerPerType = (
     : `box-shadow: ${theme.elevation['02']}
 `;
 
-export const toastContainer = (
-  type: AcceptedColorComponentTypes,
-  styleType: NotificationStyleType
-) => (theme: Theme): SerializedStyles => css`
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-  border-radius: ${rem(8)};
-  ${toastContainerPerType(type, styleType, theme)};
-`;
+export const toastContainer =
+  (type: AcceptedColorComponentTypes, styleType: NotificationStyleType) =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      display: flex;
+      flex-direction: column;
+      overflow: hidden;
+      border-radius: ${rem(8)};
+      ${toastContainerPerType(type, styleType, theme)};
+    `;
 
-export const topContainer = (type: AcceptedColorComponentTypes) => (
-  theme: Theme
-): SerializedStyles => css`
-  color: ${theme.palette.white};
-  display: flex;
-  justify-content: space-between;
-  overflow: hidden;
-  height: ${rem(58)};
-  background: ${theme.utils.getColor(type, 500, 'normal')};
-`;
+export const topContainer =
+  (type: AcceptedColorComponentTypes) =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      color: ${theme.palette.white};
+      display: flex;
+      justify-content: space-between;
+      overflow: hidden;
+      height: ${rem(58)};
+      background: ${theme.utils.getColor(type, 500, 'normal')};
+    `;
 
-export const infoContainer = () => (theme: Theme): SerializedStyles => css`
-  ${flexCenter};
-  padding: 0 ${theme.spacing.md};
-`;
+export const infoContainer =
+  () =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      ${flexCenter};
+      padding: 0 ${theme.spacing.md};
+    `;
 
-export const infoIconContainer = () => (theme: Theme): SerializedStyles => css`
-  padding-right: ${theme.spacing.sm};
-`;
+export const infoIconContainer =
+  () =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      padding-right: ${theme.spacing.sm};
+    `;
 
-export const actionIconsContainer = () => (theme: Theme): SerializedStyles => css`
-  display: flex;
-  align-items: center;
-  padding-right: ${theme.spacing.md};
-`;
+export const actionIconsContainer =
+  () =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      display: flex;
+      align-items: center;
+      padding-right: ${theme.spacing.md};
+    `;
 
-export const chevronIconContainer = (expanded: boolean) => (): SerializedStyles => css`
-  cursor: pointer;
-  transform: rotate(${expanded ? '180' : '0'}deg);
-  ${transition(0.2)}
-`;
+export const chevronIconContainer = (expanded: boolean) => (): SerializedStyles =>
+  css`
+    cursor: pointer;
+    transform: rotate(${expanded ? '180' : '0'}deg);
+    ${transition(0.2)}
+  `;
 
-export const expandedContainer = (type: AcceptedColorComponentTypes, isExpanded: boolean) => (
-  theme: Theme
-): SerializedStyles => css`
-  ${transition(0.1)};
-  min-height: ${isExpanded ? rem(146) : rem(0)};
-  ${isNotificationTypes(type) ? maxHeightOptions['notification'] : maxHeightOptions['generic']}
-  height: ${!isExpanded ? rem(0) : 'inherit'};
-  font-size: ${theme.typography.fontSizes['14']};
-  position: relative;
-  background: ${theme.palette.white};
-`;
+export const expandedContainer =
+  (type: AcceptedColorComponentTypes, isExpanded: boolean, hasMinimumHeight: boolean) =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      ${transition(0.1)};
+      min-height: ${hasMinimumHeight && isExpanded ? rem(146) : rem(0)};
+      ${isNotificationTypes(type) ? maxHeightOptions['notification'] : maxHeightOptions['generic']}
+      height: ${!isExpanded ? rem(0) : 'inherit'};
+      font-size: ${theme.typography.fontSizes['14']};
+      position: relative;
+      background: ${theme.palette.white};
+    `;
