afc46bdf1899904130da1858d617f6fff26e0696
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2023-03-30 11:45:55 +0300

    chore(documentation): adding extra demo on the documentation of storybook for the hook

--- src/hooks/storyUtils/DemoUseSearchQueryParams.tsx
+++ src/hooks/storyUtils/DemoUseSearchQueryParams.tsx
@@ -1,17 +1,21 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { withRouter, RouterProps } from 'react-router-dom';
 
 import { useSearchQueryParams } from '../';
 
-const DemoUseSearchQueryParams: FC = () => {
+const DemoUseSearchQueryParams: FC<{ initialEntries: string } & RouterProps> = (props) => {
   const searchQueryParams = useSearchQueryParams();
+  const url = decodeURIComponent(props.initialEntries.replace(/&amp;/g, '&'));
+
+  useEffect(() => {
+    props.history.push(decodeURI(url));
+  }, [props.history, url]);
 
   return (
     <div>
       <h1>useSearchQueryParams hook</h1>
       <p style={{ padding: '1.4rem', background: '#fff' }}>
-        Given the url{' '}
-        <code>https://orfium-product.com/mypath?page=1&size=12&library=react&testing=yolo</code> the
-        result of this hook is:
+        Given the url <code>{url}</code> the result of this hook is:
         <pre>
           <code>{JSON.stringify(searchQueryParams, null, 2)}</code>
         </pre>
@@ -20,4 +24,5 @@ const DemoUseSearchQueryParams: FC = () => {
   );
 };
 
-export default DemoUseSearchQueryParams;
+// @ts-ignore
+export default withRouter(DemoUseSearchQueryParams);
