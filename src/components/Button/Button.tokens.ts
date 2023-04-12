5f8c0366dafb288b3ae38b8a97131ff789f6bbf8
kostasdano <kostas.danochristos@orfium.com>
2023-04-12 09:54:28 +0300

    feat: update button tokens

--- src/components/Button/Button.tokens.ts
+++ src/components/Button/Button.tokens.ts
@@ -1,73 +1,28 @@
-import { ButtonTypes } from './Button.types';
+import button from 'theme/tokens/components/variables/button';
+import iconButton from 'theme/tokens/components/variables/iconButton';
+import textButton from 'theme/tokens/components/variables/textButton';
+import { DotKeys, getComponentTokens } from 'theme/tokens/utils';
+
 import { Theme } from '../../theme';
 
-export type ButtonTokensColors = {
-  borderColor: string;
-  borderColorSegmented?: string;
-  textColor: string;
+export type ButtonTokens = DotKeys<typeof button>;
+export type TextButtonTokens = DotKeys<typeof textButton>;
+export type IconButtonTokens = DotKeys<typeof iconButton>;
+
+export const getButtonTokens = (
+  theme: Theme
+): ((path: ButtonTokens, fn?: (val: string) => any) => string) => {
+  return getComponentTokens(button, theme);
 };
 
-export type ButtonTokens = {
-  size: string;
-  spacing: {
-    textButton: { padding: string; paddingVertical: string; paddingHorizontal: string };
-    iconButton: Record<string, string>;
-  };
-  borderRadius: { text: string; wrapper: string; icon: string };
-  borderWidth: Record<string, string>;
-  color: Record<ButtonTypes, ButtonTokensColors>;
+export const getTextButtonTokens = (
+  theme: Theme
+): ((path: TextButtonTokens, fn?: (val: string) => any) => string) => {
+  return getComponentTokens(textButton, theme);
 };
 
-const getTokens = (theme: Theme): ButtonTokens => {
-  return {
-    size: theme.globals.sizing.get('5'),
-    spacing: {
-      textButton: {
-        padding: theme.globals.spacing.get('4'),
-        paddingVertical: theme.globals.spacing.get('4'),
-        paddingHorizontal: theme.globals.spacing.get('6'),
-      },
-      iconButton: {
-        padding: theme.globals.spacing.get('4'),
-      },
-    },
-    borderRadius: {
-      text: theme.globals.borderRadius.get('2'),
-      wrapper: theme.globals.borderRadius.get('0'),
-      icon: theme.globals.borderRadius.get('5'),
-    },
-    borderWidth: {
-      1: theme.globals.borderWidth.get('1'),
-      2: theme.globals.borderWidth.get('2'),
-    },
-    color: {
-      primary: {
-        borderColor: theme.tokens.borderColor.get('decorative.transparent'),
-        textColor: theme.tokens.textColor.get('inverted.primary'),
-      },
-      secondary: {
-        borderColor: theme.tokens.borderColor.get('decorative.transparent'),
-        textColor: theme.tokens.textColor.get('light.active'),
-      },
-      tertiary: {
-        borderColor: theme.tokens.borderColor.get('decorative.transparent'),
-        borderColorSegmented: theme.tokens.borderColor.get('decorative.light.muted'),
-        textColor: theme.tokens.textColor.get('light.active'),
-      },
-      invertedAlt: {
-        borderColor: theme.tokens.borderColor.get('decorative.transparent'),
-        textColor: theme.tokens.textColor.get('inverted.active'),
-      },
-      danger: {
-        borderColor: theme.tokens.borderColor.get('decorative.transparent'),
-        textColor: theme.tokens.textColor.get('light.error'),
-      },
-      inverted: {
-        borderColor: theme.tokens.borderColor.get('decorative.transparent'),
-        textColor: theme.tokens.textColor.get('light.primary'),
-      },
-    },
-  };
+export const getIconButtonTokens = (
+  theme: Theme
+): ((path: IconButtonTokens, fn?: (val: string) => any) => string) => {
+  return getComponentTokens(iconButton, theme);
 };
-
-export default getTokens;
