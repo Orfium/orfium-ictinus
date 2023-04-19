2d4a4ee6aa37b7767da567e83dbc98012f75f1d6
kostasdano <kostas.danochristos@orfium.com>
2023-04-19 15:55:26 +0300

    feat: create MultiSelectBase component

---
+++ src/components/MultiSelectBase/MultiSelectBase.style.ts
@@ -0,0 +1,127 @@
+import { css, SerializedStyles } from '@emotion/react';
+import { rem } from 'polished';
+import { Theme } from 'theme';
+import { getTextFieldHeight } from 'utils/size-utils';
+
+import { LABEL_TRANSFORM_LEFT_SPACING } from 'components/Label/Label.style';
+
+export const chipContent =
+  ({ maxWidth }: { maxWidth?: number }) =>
+  (): SerializedStyles =>
+    css`
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      max-width: ${maxWidth ? rem(maxWidth - 120) : 'unset'};
+    `;
+
+export const textFieldContainer =
+  () =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      margin: ${theme.spacing.xsm} ${theme.spacing.sm} 0 0;
+    `;
+
+export const rightIconsContainer =
+  () =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      position: absolute !important;
+      bottom: 0;
+      right: ${theme.spacing.md};
+      top: 0;
+      display: flex;
+      align-items: center;
+    `;
+
+export const rightIconStyles =
+  ({ isClickable }: { isClickable: boolean }) =>
+  (): SerializedStyles =>
+    css`
+      cursor: ${isClickable ? 'pointer' : 'auto'};
+    `;
+
+export const chipStyle =
+  () =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      position: relative;
+      margin-right: ${theme.spacing.xsm};
+      & > div {
+        height: ${rem(20)};
+        border-radius: ${rem(4)};
+      }
+    `;
+
+export const inputContainer =
+  () =>
+  (theme: Theme): SerializedStyles =>
+    css`
+      position: static;
+      width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      row-gap: ${theme.spacing.xsm};
+      z-index: 2;
+    `;
+
+/** TextField overrides */
+
+export const textInputBaseOverrides =
+  ({
+    hasValue,
+    isLoading,
+    hasLabel,
+  }: {
+    hasValue: boolean;
+    isLoading?: boolean;
+    hasLabel?: boolean;
+  }) =>
+  (theme: Theme) => {
+    const labelStyles = {
+      fontWeight: `${theme.typography.weights.bold} !important` as any,
+      transform: `translate(${LABEL_TRANSFORM_LEFT_SPACING}, -82%) scale(0.8) !important`,
+      bottom: 'auto',
+    };
+
+    return {
+      wrapper: {
+        height: 'unset',
+        minHeight: `${getTextFieldHeight('md')} !important`,
+        ...(hasValue
+          ? { label: labelStyles }
+          : {
+              'input:focus': {
+                label: labelStyles,
+              },
+              label: {
+                transform: `translate(${LABEL_TRANSFORM_LEFT_SPACING}, -8px)`,
+              },
+            }),
+      },
+      textField: {
+        // For the paddings we use specifically custom numbers that don't match the
+        // ictinus spacing (the are calculated by combining internal absolute positioned component widths)
+        // so we can override the existing TextFieldInputBase paddings to
+        // perfectly position and align the content inside.
+        padding: isLoading
+          ? `${rem(hasLabel ? 21 : 13)} ${rem(80)} ${rem(hasLabel ? 5 : 13)} ${theme.spacing.md}`
+          : `${rem(hasLabel ? 21 : 13)} ${rem(40)} ${rem(hasLabel ? 5 : 13)} ${theme.spacing.md}`,
+      },
+    };
+  };
+
+export const inputOverrides = () => {
+  return {
+    input: {
+      top: 'unset',
+      flex: '1 1 0%',
+      minWidth: '20%',
+      '&:focus, &:not(:placeholder-shown)': {
+        '& + label': {
+          transform: `translate(${LABEL_TRANSFORM_LEFT_SPACING}, -82%) scale(0.8)`,
+        },
+      },
+    },
+  };
+};
---
+++ src/components/MultiSelectBase/MultiSelectBase.tsx
@@ -0,0 +1,168 @@
+import useTheme from 'hooks/useTheme';
+import omit from 'lodash/omit';
+import React, { useMemo } from 'react';
+import { generateUniqueID } from 'utils/helpers';
+import { TestProps } from 'utils/types';
+
+import useMultiSelectBaseUtils from './hooks';
+import {
+  chipContent,
+  chipStyle,
+  inputContainer,
+  inputOverrides,
+  rightIconsContainer,
+  rightIconStyles,
+  textInputBaseOverrides,
+} from './MultiSelectBase.style';
+import Chip from 'components/Chip';
+import Label from 'components/Label';
+import Loader from 'components/Loader';
+import { SelectOption } from 'components/Select/Select';
+import { InputProps, Props as TextFieldProps } from 'components/TextField/TextField';
+import TextInputBase from 'components/TextInputBase';
+import { inputStyle } from 'components/TextInputBase/TextInputBase.style';
+
+type Props = {
+  /** the values of the MultiSelect if MultiSelect is controlled */
+  selectedOptions?: SelectOption[];
+  /** Callback fired when the `input` value typed is changed */
+  onInput?: React.EventHandler<any>;
+  /** Value of the `input` element */
+  value?: string | number;
+  /** Callback fired when the Delete button of each Chip is clicked */
+  onOptionDelete: (option?: SelectOption) => void;
+  /** Callback fired when the Delete button of the whole MultiSelect is clicked */
+  onClearAllOptions: () => void;
+  /** If the component is loading */
+  isLoading?: boolean;
+  /** Whether the Textfield should change its styles when hovered/focused etc */
+  isInteractive?: boolean;
+} & Omit<TextFieldProps, 'size'>;
+
+const MultiSelectBase = React.forwardRef<HTMLInputElement, Props & InputProps & TestProps>(
+  (props, ref) => {
+    const {
+      selectedOptions,
+      value,
+      disabled,
+      locked,
+      status,
+      lean,
+      dark,
+      readOnly,
+      label,
+      id,
+      placeholder,
+      required = false,
+      styleType,
+      onOptionDelete,
+      onClearAllOptions,
+      isLoading,
+      rightIcon,
+      isInteractive = true,
+      ...rest
+    } = props;
+
+    const theme = useTheme();
+    const hasValue = Boolean(value || (selectedOptions?.length && selectedOptions?.length > 0));
+
+    const { inputPlaceholder, handleKeyDown, icon, hasLabel, TextfieldRef } =
+      useMultiSelectBaseUtils({
+        label,
+        placeholder,
+        required,
+        locked,
+        hasValue,
+        value,
+        rightIcon,
+        onOptionDelete,
+        onClearAllOptions,
+      });
+
+    const chips = useMemo(
+      () => (
+        <>
+          {selectedOptions?.map((option, index) => (
+            <span key={generateUniqueID('chip' + index)} css={chipStyle()}>
+              <Chip
+                onClear={!(locked || disabled) ? () => onOptionDelete(option) : undefined}
+                fill="lightGrey"
+                dataTestId={`chip_${index}`}
+              >
+                <div
+                  title={option.label}
+                  css={chipContent({
+                    maxWidth: TextfieldRef.current?.getBoundingClientRect().width,
+                  })}
+                >
+                  {option.label}
+                </div>
+              </Chip>
+            </span>
+          ))}
+        </>
+      ),
+      [disabled, locked, onOptionDelete, selectedOptions]
+    );
+
+    return (
+      <div ref={TextfieldRef}>
+        <TextInputBase
+          disabled={isInteractive && disabled}
+          locked={isInteractive && locked}
+          status={isInteractive ? status : 'normal'}
+          lean={lean}
+          size={'md'}
+          styleType={styleType}
+          {...rest}
+          isInteractive={isInteractive}
+          sx={textInputBaseOverrides({ hasValue, isLoading, hasLabel })(theme)}
+        >
+          <div css={inputContainer()}>
+            {chips}
+            <input
+              readOnly={readOnly}
+              onKeyDown={handleKeyDown}
+              css={inputStyle({
+                size: 'md',
+                placeholder,
+                label,
+                dark,
+                lean,
+                sx: inputOverrides(),
+              })}
+              placeholder={inputPlaceholder}
+              required={required}
+              id={id}
+              disabled={disabled || locked}
+              {...omit(rest, 'dataTestId')}
+              value={value}
+              ref={ref}
+            />
+            {label && (
+              <Label
+                size={'md'}
+                htmlFor={id}
+                label={label}
+                required={required}
+                animateToTop={hasValue}
+                error={status === 'error'}
+              />
+            )}
+          </div>
+
+          {!disabled && (
+            <div css={rightIconsContainer()}>
+              {isLoading && <Loader />}
+              <div css={rightIconStyles({ isClickable: hasValue && !locked })}>{icon}</div>
+            </div>
+          )}
+        </TextInputBase>
+      </div>
+    );
+  }
+);
+
+MultiSelectBase.displayName = 'MultiSelectBase';
+
+export default MultiSelectBase;
