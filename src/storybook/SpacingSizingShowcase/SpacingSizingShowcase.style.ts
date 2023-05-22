1b74008f2ce3bfb4480f86b67188bd19a6dd6b9a
whosyourtaco <george.metakos@orfium.com>
2023-05-22 20:14:21 +0300

    feat: updates Spacing documentation page

--- src/storybook/SpacingSizingShowcase/SpacingSizingShowcase.style.ts
+++ src/storybook/SpacingSizingShowcase/SpacingSizingShowcase.style.ts
@@ -1,23 +1,17 @@
 import { css, SerializedStyles } from '@emotion/react';
 
-export const WrapperStyle = (): SerializedStyles => css`
+import { Theme } from '../../theme';
+
+export const WrapperStyle = (theme: Theme): SerializedStyles => css`
   display: flex;
   flex-direction: row;
-  align-items: flex-end;
   border: 1px solid #dedede;
   border-radius: 10px;
   padding: 40px 15px;
-
-  > div {
-    margin: 0 5px;
-    text-align: center;
-
-    > div:nth-child(2) {
-      margin: 10px 0;
-    }
-
-    > div:last-of-type {
-      font-size: 0.8rem;
-    }
-  }
+  row-gap: 20px;
+  column-gap: 64px;
+  flex-wrap: wrap;
+  align-items: start;
+  justify-content: start;
+  background: ${theme.tokens.backgroundColor.get('tintedDark')};
 `;
--- src/storybook/SpacingSizingShowcase/SpacingSizingShowcase.tsx
+++ src/storybook/SpacingSizingShowcase/SpacingSizingShowcase.tsx
@@ -1,30 +1,82 @@
 import { map } from 'lodash';
-import { rem } from 'polished';
 import React from 'react';
 import spacing from 'theme/globals/constants/spacing';
 
 import { WrapperStyle } from './SpacingSizingShowcase.style';
+import { useTheme } from '../../index';
 
 const SpacingSizingShowcase = () => {
   const spaces = map(spacing);
+  const theme = useTheme();
 
   return (
     <div css={WrapperStyle}>
       {spaces.map((space, index) => (
         <div
-          key={space.value}
-          style={{
-            width: `calc(100%/${spaces.length})`,
+          key={`spacing_${index}`}
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '16px',
           }}
         >
           <div
-            style={{
-              height: rem(`${space.value}px`),
-              background: '#1ee9b5',
+            css={{
+              display: 'flex',
+              justifyContent: 'start',
+              alignItems: 'start',
+              width: '108px',
             }}
-          />
-          <div>{space.value}px</div>
-          <div>spacing.{index}</div>
+          >
+            <div
+              css={{
+                width: '32px',
+                height: '32px',
+                borderRadius: '50%',
+                backgroundColor: theme.tokens.palette.get('systemic.secondary.dark'),
+              }}
+            />
+            <div
+              css={{
+                width: `${space.value}px`,
+                height: '32px',
+                backgroundColor: theme.utils.getColor('lightPurple', 550),
+              }}
+            />
+            <div
+              css={{
+                width: '32px',
+                height: '32px',
+                borderRadius: '50%',
+                backgroundColor: theme.tokens.palette.get('systemic.secondary.dark'),
+              }}
+            />
+          </div>
+          <div
+            css={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '8px',
+            }}
+          >
+            <span
+              css={{
+                fontWeight: theme.globals.typography.fontWeight.get('medium'),
+                color: theme.tokens.textColor.get('light.primary'),
+              }}
+            >
+              spacing.{index}
+            </span>
+            <span
+              css={{
+                color: theme.tokens.textColor.get('light.secondary'),
+                fontSize: '14px',
+                fontWeight: theme.globals.typography.fontWeight.get('regular'),
+              }}
+            >
+              {space.value}px
+            </span>
+          </div>
         </div>
       ))}
     </div>
