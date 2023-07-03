80d57ef73d08bf7d2e4eb69937be07703156aca7
kostasdano <kostas.danochristos@orfium.com>
2023-07-03 15:50:24 +0300

    feat: new textarea

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import isEqual from 'react-fast-compare';
 
-import { sxProp } from './TextArea.style';
-import { useTheme } from '../../index';
+import { hintMessageStyle, sxProp } from './TextArea.style';
+import { Label, useTheme } from '../../index';
 import { TestProps } from '../../utils/types';
 import TextInputBase, { TextInputBaseProps } from '../TextInputBase/TextInputBase';
 import { inputStyle as baseInputStyle } from 'components/TextInputBase/TextInputBase.style';
@@ -10,14 +10,20 @@ import { inputStyle as baseInputStyle } from 'components/TextInputBase/TextInput
 export type TextAreaProps = {
   /** The id of the text field that will be used as for in label too */
   id?: string;
+  /** The value of the textarea element */
+  value?: string;
   /** The placeholder of the input that will be used. This is shown if no label exists */
   placeholder?: string;
   /** If the text field value is required */
   isRequired?: boolean;
+  /** Boolean to make the input readonly. Default to false. */
+  isReadOnly?: boolean;
   /** If the text field is disabled */
   isDisabled?: boolean;
   /** If the text area can be resized */
   isResizeEnabled?: boolean;
+  /** Number of maximum characters. Will be shown on the counter on the bottom right of the component */
+  maxCharacters?: number;
   /** Callback fired when the `input` is blurred. */
   onBlur?: React.FocusEventHandler<HTMLInputElement | HTMLTextAreaElement>;
   /** Callback fired when the `input` is changed. */
@@ -28,40 +34,70 @@ export type TextAreaProps = {
   onKeyDown?: React.KeyboardEventHandler<HTMLTextAreaElement | HTMLInputElement>;
   /** Callback fired when the `input` value typed is changed */
   onInput?: React.EventHandler<any>;
-} & Pick<TextInputBaseProps, 'status'> &
+} & Pick<HTMLTextAreaElement, 'cols' | 'rows'> &
+  Pick<TextInputBaseProps, 'status' | 'label'> &
   TestProps;
 
 const TextArea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>((props, ref) => {
   const {
     id = undefined,
-    placeholder = '',
+    label,
+    placeholder,
     isRequired = false,
+    isReadOnly,
     isDisabled,
+    status,
     isResizeEnabled = true,
+    maxCharacters,
     ...rest
   } = props;
 
+  const isLocked = status?.type === 'read-only';
+
   const theme = useTheme();
-  const sx = sxProp(!isDisabled && isResizeEnabled, theme);
+  const sx = sxProp({
+    isResizeEnabled: !isDisabled && !isLocked && isResizeEnabled,
+    label,
+    placeholder,
+  })(theme);
+
+  const shouldShowCounter = maxCharacters && status?.type != 'error' && !status?.hintMessage;
+
+  const counter = shouldShowCounter ? (
+    <div css={hintMessageStyle(theme)}>
+      {rest.value?.length}/{maxCharacters}
+    </div>
+  ) : undefined;
 
   return (
     <React.Fragment>
       <TextInputBase {...props} sx={sx}>
-        <div css={{ width: '100% ' }}>
+        <div css={{ width: '100%' }}>
           <textarea
+            readOnly={isLocked || isReadOnly}
             css={baseInputStyle({
               placeholder,
+              label,
               sx,
             })}
-            placeholder={placeholder}
+            placeholder={placeholder ? `${placeholder} ${isRequired ? '*' : ''}` : label}
             required={isRequired}
             id={id}
-            disabled={isDisabled}
+            maxLength={maxCharacters}
+            disabled={isDisabled || isLocked}
             {...rest}
             ref={ref}
           />
+          <Label
+            htmlFor={id}
+            label={label}
+            isRequired={isRequired}
+            isAnimated={Boolean(rest.value)}
+            hasError={status?.type === 'error'}
+          />
         </div>
       </TextInputBase>
+      {counter}
     </React.Fragment>
   );
 });
--- src/components/TextInputBase/config.ts
+++ src/components/TextInputBase/config.ts
@@ -1,3 +1,4 @@
 export const MIN_WIDTH = 140;
 
 export const MULTI_MIN_WIDTH = 240;
+export const TEXT_AREA_MIN_WIDTH = 220;
