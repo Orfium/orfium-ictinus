5a934e0d3f01cd3931207fc9ffac71afd3571f77
kostasdano <kostas.danochristos@orfium.com>
2023-07-25 12:58:51 +0300

    feat: revert changes and add documentation

--- src/components/Drawer/Drawer.style.ts
+++ src/components/Drawer/Drawer.style.ts
@@ -7,6 +7,7 @@ import { Theme } from '../../theme';
 export const drawerContainerStyle = (
   expanded: boolean,
   isDesktop: boolean,
+  isSmallDesktop: boolean
 ) => (theme: Theme): SerializedStyles => css`
   ${transition(0.2)};
   width: ${expanded ? rem('308px') : isDesktop ? rem('112px') : rem('0px')};
@@ -17,6 +18,6 @@ export const drawerContainerStyle = (
   height: 100%;
   min-height: 100%;
   z-index: 100;
-  position: relative;
+  position: ${isSmallDesktop ? 'absolute' : 'relative'};
   border-right: ${rem(1)} solid ${theme.utils.getColor('lightGrey', 200)};
 `;
