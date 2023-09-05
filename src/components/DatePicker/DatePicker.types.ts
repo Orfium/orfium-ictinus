cf772da9b7a7e4a1f63eab631c1e4f050931ee15
kostasdano <kostas.danochristos@orfium.com>
2023-09-05 10:23:51 +0300

    feat: reduce DatePicker file size

---
+++ src/components/DatePicker/DatePicker.types.ts
@@ -0,0 +1,56 @@
+import { Dayjs } from 'utils/date';
+
+import { FilterType, StyleType } from '../Filter/types';
+import { TextFieldProps } from '../TextField/TextField';
+
+export type DisabledDates = {
+  daysOfWeek?: number[];
+  days?: Date[];
+  after?: Date;
+  before?: Date;
+};
+
+export type DateRange = { from: Date | undefined; to: Date | undefined };
+
+export type DatePickerProps = {
+  /** This property is to define if this is a day picker or a day range picker */
+  isRangePicker?: boolean;
+  /** A callback to return user selection */
+  onChange?: (range: DateRange) => void;
+  /** Option to disable some dates */
+  disableDates?: DisabledDates;
+  /** Value to define if needed an initial state or to handle it externally */
+  value: DateRange;
+  /** Props of the TextField input */
+  inputProps?: TextFieldProps;
+  /** The format of the date displayed in the input field */
+  dateFormatOverride?: DateFormatType;
+  /** if the datepicker can be clear with backspace */
+  isClearable?: boolean;
+  /** if the datepicker's default date is today instead of placeholder text */
+  /** @deprecated This prop is being deprecated and is not used as you can manipulate date from value, will be removed in the future **/
+  isDefaultNow?: boolean;
+  /** Style properties for the DatePicker with a filter base */
+  filterConfig?: {
+    /** The type of the filter button's palette - defaults to "primary" */
+    buttonType?: 'primary' | 'secondary';
+    /** Defines the style type of the filter button */
+    styleType?: StyleType;
+    /** This property defines the Filter's type */
+    filterType?: FilterType;
+  };
+  /** Whether the DateRangePicker has options */
+  hasOptions?: boolean;
+};
+
+export type ExtraOption = { value: string; label: string; dates: Dayjs[] };
+
+export type DateFormatType =
+  | 'MM/DD/YYYY'
+  | 'DD/MM/YYYY'
+  | 'MMMM D, YYYY'
+  | 'dddd, MMMM D, YYYY'
+  | 'M/D/YYYY'
+  | 'MMM D, YYYY'
+  | 'ddd, MMM D, YYYY'
+  | 'DD MMM YYYY';
