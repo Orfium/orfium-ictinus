834963d36860b7e8233363969698ff3cb619e4c2
kostasdano <kostas.danochristos@orfium.com>
2023-09-14 17:25:46 +0300

    feat: keyboard navigation for DatePickInput

--- src/components/DatePicker/DatePickInput/DatePickInput.style.ts
+++ src/components/DatePicker/DatePickInput/DatePickInput.style.ts
@@ -1,6 +1,23 @@
 import { css, SerializedStyles } from '@emotion/react';
+import { rem } from 'polished';
+import { Theme } from 'theme';
 
-export const iconStyles = (): SerializedStyles =>
-  css`
-    cursor: pointer;
-  `;
+export const iconStyles =
+  () =>
+  (theme: Theme): SerializedStyles => {
+    return css`
+      cursor: pointer;
+      /** @TODO: revisit these styles when Interactive Icon is implemented */
+      width: ${rem(36)};
+      height: ${rem(36)};
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      /** @TODO: revisit these styles when Interactive Icon is implemented */
+      &:focus-visible {
+        background: ${theme.tokens.state.get('backgroundColor.hover')};
+        border-radius: ${theme.globals.borderRadius.get('7')};
+      }
+    `;
+  };
