3f4e40b3e448fc761e529c37a6f22330971770ad
kostasdano <kostas.danochristos@orfium.com>
2023-10-02 03:03:57 +0300

    feat: update typography tokens parsing

--- src/components/Typography/utils.ts
+++ src/components/Typography/utils.ts
@@ -1,11 +1,13 @@
 import { css } from '@emotion/react';
-import { SemanticTypographyKeys } from 'theme/tokens/semantic/typography';
+import { TypographyKeys } from 'theme/tokens/semantic/typography';
 
 /** Generates a css object with the values included in the value object */
-export const generateStylesFromTokens = (value: Record<SemanticTypographyKeys, string>) => css`
+export const generateStylesFromTokens = (value: Record<TypographyKeys, string>) => css`
   font-family: ${value.fontFamily};
   font-weight: ${value.fontWeight};
   line-height: ${value.lineHeight};
   font-size: ${value.fontSize};
   letter-spacing: ${value.letterSpacing};
+  text-transform: ${value?.textCase};
+  text-decoration: ${value?.textDecoration};
 `;
