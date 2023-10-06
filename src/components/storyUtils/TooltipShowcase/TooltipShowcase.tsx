652a595c4aa7e66e0e7199ddb4735c16845007f8
kostasdano <kostas.danochristos@orfium.com>
2023-10-06 10:33:34 +0300

    feat: stories and showcases

--- src/components/storyUtils/TooltipShowcase/TooltipShowcase.tsx
+++ src/components/storyUtils/TooltipShowcase/TooltipShowcase.tsx
@@ -1,122 +1,47 @@
-import { css } from '@emotion/react';
-import { boolean, number } from '@storybook/addon-knobs';
 import React from 'react';
 
-import {
-  customContentStyle,
-  tooltipContainerStyle,
-  tooltipRowStyle,
-} from './TooltipShowcase.style';
-import { rem } from '../../../theme/utils';
-import Button from '../../Button';
-import Card from '../../Card';
-import Chip from '../../Chip';
 import Tooltip from '../../Tooltip';
+import Button from 'components/Button';
+import { TooltipProps } from 'components/Tooltip/Tooltip.types';
 
-const CustomContent = () => {
+const TooltipShowcase = ({
+  buttonText = 'Hover here',
+  isInverted = false,
+  delayIn,
+  delayOut,
+  placement = 'right',
+}: Pick<TooltipProps, 'isInverted' | 'delayIn' | 'delayOut' | 'placement'> & {
+  buttonText?: string;
+}) => {
   return (
-    <Card elevated="02" radius="3">
-      <div css={customContentStyle()}>
-        <div
-          css={css`
-            margin-bottom: ${rem(8)};
-            color: black;
-          `}
-        >
-          By hover here you can see:
-        </div>
-        <Chip fill="teal">
-          <a href={'/'}>Go to homepage</a>
-        </Chip>
-      </div>
-    </Card>
-  );
-};
-
-const TooltipShowcase = ({ content }: { content: string }) => {
-  return (
-    <div css={tooltipContainerStyle()}>
-      <h2>Tooltip with text-only content</h2>
-      <div css={tooltipRowStyle()}>
-        <Tooltip
-          content={content}
-          placement={'left'}
-          isInteractive={boolean('isInteractive', false)}
-          delay={number('delay', 100)}
-        >
-          <Button
-            onClick={() =>
-              console.log(
-                'The C language combines all the power of assembly language with all the ease-of-use of assembly language.'
-              )
-            }
-          >
-            Left
-          </Button>
-        </Tooltip>
-        <Tooltip
-          content={content}
-          placement={'right'}
-          isInteractive={boolean('isInteractive', false)}
-          delay={number('delay', 100)}
-        >
-          <Button
-            onClick={() =>
-              console.log(
-                'A SQL query goes into a bar, walks up to two tables and asks, “Can I join you?'
-              )
-            }
-          >
-            Right
-          </Button>
-        </Tooltip>
-      </div>
-
-      <div css={tooltipRowStyle()}>
-        <Tooltip
-          content={content}
-          placement={'top'}
-          isInteractive={boolean('isInteractive', false)}
-          delay={number('delay', 100)}
-        >
-          <Button
-            onClick={() =>
-              console.log(
-                'How do you tell HTML from HTML5? Try it out in Internet Explorer. Did it work? No? It’s HTML5.'
-              )
-            }
-          >
-            Top
-          </Button>
-        </Tooltip>
-        <Tooltip
-          content={content}
-          placement={'bottom'}
-          isInteractive={boolean('isInteractive', false)}
-          delay={number('delay', 100)}
-        >
-          <Button
-            onClick={() =>
-              console.log('Knock knock.\n' + '\n' + 'Race condition.\n' + '\n' + 'Who’s there?')
-            }
-          >
-            Bottom
-          </Button>
-        </Tooltip>
-      </div>
-      <h2>Tooltip with component content</h2>
+    <div style={{ padding: '48px' }}>
       <Tooltip
-        isTransparent
-        content={CustomContent()}
-        placement={'right'}
-        isInteractive={boolean('isInteractive', false)}
-        delay={number('delay', 100)}
+        delayIn={delayIn}
+        delayOut={delayOut}
+        placement={placement}
+        content={
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              background: isInverted ? '#DCDFFF' : '#434666',
+              padding: '8px',
+              width: '240px',
+              color: isInverted ? 'black' : 'white',
+              border: '1px solid' + isInverted ? '#DCDFFF' : '#434666',
+              borderRadius: '4px',
+            }}
+          >
+            <div>This container is custom content</div>
+            <Button>Hello there mate</Button>
+          </div>
+        }
+        isInteractive
+        isInverted={isInverted}
       >
-        <Button
-          onClick={() => console.log('Hardware (noun): the part of a computer that you can kick.')}
-        >
-          Right
-        </Button>
+        <Button>{buttonText}</Button>
       </Tooltip>
     </div>
   );
