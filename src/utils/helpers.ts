04dc26fe47f65c95d1aaac6de01d2c1df9a15e87
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2023-10-11 17:55:10 +0300

    fix: all security deps from v4.5 to next

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,7 +4,10 @@ import { TestId } from 'utils/types';
 
 /** A function that generates a unique id by making a value randomly based on time also */
 export const generateUniqueID = (elementType = ''): string =>
-  elementType + '_' + (Date.now() + Math.random()).toString(36).substr(2, 9);
+  elementType +
+  (process.env.NODE_ENV !== 'test'
+    ? '_' + (Date.now() + Math.random()).toString(36).substr(2, 9)
+    : '');
 
 /** A function that takes two strings to generate a test data id by combining them if both exist */
 export const generateTestDataId = (defaultId: TestId, customId?: TestId) =>
