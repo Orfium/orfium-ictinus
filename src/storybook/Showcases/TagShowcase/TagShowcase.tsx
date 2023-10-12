37beda148bad43c90045977aadb5c2c59a04c11b
kostasdano <kostas.danochristos@orfium.com>
2023-10-12 17:46:26 +0300

    fix: stories

---
+++ src/storybook/Showcases/TagShowcase/TagShowcase.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from 'react';
+
+import Tag, { TagProps } from 'components/Tag';
+
+type Props = Pick<TagProps, 'color' | 'size' | 'iconName'> & {
+  hasIcon?: boolean;
+  text: string;
+  type: 'read-only' | 'clearable' | 'selectable';
+};
+
+const TagShowcase: FC<Props> = ({ color, size, hasIcon, iconName, type, text }) => {
+  const [isSelected, setIsSelected] = useState(false);
+
+  const restProps =
+    type === 'clearable'
+      ? {
+          onClear: () => console.log('clear'),
+        }
+      : type === 'selectable'
+      ? {
+          isSelected,
+          onSelect: () => setIsSelected(true),
+        }
+      : {
+          ...(hasIcon ? { iconName } : {}),
+        };
+
+  return (
+    <div>
+      <Tag color={color} size={size} {...restProps}>
+        {text}
+      </Tag>
+    </div>
+  );
+};
+
+export default TagShowcase;
