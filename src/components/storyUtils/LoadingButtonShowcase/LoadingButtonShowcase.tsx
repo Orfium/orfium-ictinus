1bd671810fe5f09c19a73d5b3ae05becac52922d
kostasdano <kostas.danochristos@orfium.com>
2023-10-24 02:50:48 +0300

    feat: storybook

--- src/components/storyUtils/LoadingButtonShowcase/LoadingButtonShowcase.tsx
+++ src/components/storyUtils/LoadingButtonShowcase/LoadingButtonShowcase.tsx
@@ -6,38 +6,78 @@ import { showcaseContainerStyle } from '../CardShowcase/CardShowcase.style';
 
 const LoadingButtonShowcase: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingCompact, setIsLoadingCompact] = useState(false);
 
-  const handleClick = () => {
-    setIsLoading(true);
+  const handleClick = (setIsLoadingValue: React.Dispatch<React.SetStateAction<boolean>>) => () => {
+    setIsLoadingValue(true);
     setTimeout(() => {
-      setIsLoading(false);
+      setIsLoadingValue(false);
     }, 4000);
   };
 
   const CardContent = () => (
     <div css={{ display: 'flex', flexDirection: 'column', gap: '48px', alignItems: 'center' }}>
       <div css={{ display: 'flex', gap: '32px' }}>
-        <Button isLoading>Primary</Button>
-        <Button type="secondary" isLoading>
-          Secondary
-        </Button>
-        <Button type="tertiary" isLoading>
-          Tertiary
-        </Button>
-        <Button type="danger" isLoading>
-          Danger
-        </Button>
-        <Button type="inverted" isLoading>
-          Inverted
-        </Button>
-        <Button type="invertedAlt" isLoading>
-          InvertedAlt
-        </Button>
+        <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '24px' }}>
+          <Button isLoading>Primary</Button>
+          <Button size="compact" isLoading>
+            Primary
+          </Button>
+        </div>
+        <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '24px' }}>
+          <Button type="secondary" isLoading>
+            Secondary
+          </Button>
+          <Button size="compact" type="secondary" isLoading>
+            Secondary
+          </Button>
+        </div>
+        <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '24px' }}>
+          <Button type="tertiary" isLoading>
+            Tertiary
+          </Button>
+          <Button size="compact" type="tertiary" isLoading>
+            Tertiary
+          </Button>
+        </div>
+        <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '24px' }}>
+          <Button type="danger" isLoading>
+            Danger
+          </Button>
+          <Button size="compact" type="danger" isLoading>
+            Danger
+          </Button>
+        </div>
+        <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '24px' }}>
+          <Button type="inverted" isLoading>
+            Inverted
+          </Button>
+          <Button size="compact" type="inverted" isLoading>
+            Inverted
+          </Button>
+        </div>
+        <div css={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '24px' }}>
+          <Button type="invertedAlt" isLoading>
+            InvertedAlt
+          </Button>
+          <Button size="compact" type="invertedAlt" isLoading>
+            InvertedAlt
+          </Button>
+        </div>
       </div>
       <div>Press the Button below to trigger loading for 4s</div>
-      <Button isLoading={isLoading} onClick={handleClick}>
-        Click here
-      </Button>
+      <div css={{ display: 'flex', gap: '32px', alignItems: 'flex-end' }}>
+        <Button isLoading={isLoading} onClick={handleClick(setIsLoading)}>
+          Click here
+        </Button>
+        <Button
+          size="compact"
+          isLoading={isLoadingCompact}
+          onClick={handleClick(setIsLoadingCompact)}
+        >
+          Click here
+        </Button>
+      </div>
     </div>
   );
 
--- src/utils/stories.ts
+++ src/utils/stories.ts
@@ -6,8 +6,8 @@ import iconSelector from 'components/Icon/assets/iconSelector';
 export const getIconSelectorKnob = (propName: string, initialValue?: AcceptedIconNames) =>
   select(
     propName,
-    Object.keys(iconSelector)
+    Object.keys({ ...iconSelector, '': '' })
       .sort((a, b) => a.localeCompare(b))
       .map((iconName) => iconName),
-    initialValue ?? 'check'
+    initialValue
   );
