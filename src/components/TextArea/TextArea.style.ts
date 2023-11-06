fcaa99e7e6659cee30546e1f67f37a13ac72f541
kostasdano <kostas.danochristos@orfium.com>
2023-11-06 17:17:35 +0200

    fix: textarea auto sizing

--- src/components/TextArea/TextArea.style.ts
+++ src/components/TextArea/TextArea.style.ts
@@ -1,8 +1,6 @@
 import { CSSObject } from '@emotion/serialize';
 
 import { Theme } from '../../theme';
-import { rem } from '../../theme/utils';
-import { MIN_WIDTH } from '../TextInputBase/config';
 
 export const sxProp = (
   resizeEnabled: boolean,
@@ -11,8 +9,7 @@ export const sxProp = (
   wrapper: { width: 'auto', height: 'auto' },
   textField: { padding: theme.spacing.sm },
   input: {
-    width: rem(MIN_WIDTH),
-    minWidth: rem(MIN_WIDTH),
+    maxWidth: '100%',
     resize: !resizeEnabled ? 'none' : 'both',
   },
 });
