0592265397d5c0e67fcea990f06b20804e44f032
kostasdano <kostas.danochristos@orfium.com>
2023-11-21 13:05:03 +0200

    feat: refactor useBreakpoints

--- src/hooks/useBreakpoints.tsx
+++ src/hooks/useBreakpoints.tsx
@@ -1,5 +1,4 @@
-import toPairs from 'lodash/toPairs';
-import { useMedia } from 'react-media';
+import { useMediaQuery } from 'react-responsive';
 
 export const queriesKeys = [
   'des1920',
@@ -13,26 +12,35 @@ export const queriesKeys = [
   'mob320',
 ] as const;
 
-export const queriesSizes: Record<typeof queriesKeys[number], string> = {
-  des1920: '1920',
-  des1440: '1440',
-  des1366: '1366',
-  des1200: '1200',
-  tab1024: '1024',
-  tab970: '970',
-  tab750: '750',
-  mob480: '480',
-  mob320: '320',
+export const queriesSizes: Record<(typeof queriesKeys)[number], number> = {
+  des1920: 1920,
+  des1440: 1440,
+  des1366: 1366,
+  des1200: 1200,
+  tab1024: 1024,
+  tab970: 970,
+  tab750: 750,
+  mob480: 480,
+  mob320: 320,
 };
 
-const queries = toPairs(queriesSizes).reduce((acc, size) => {
-  acc[size[0]] = `(min-width: ${size[1]}px)`;
+const useIctinusMediaQuery = (key: (typeof queriesKeys)[number]) =>
+  useMediaQuery({ minWidth: queriesSizes[key] });
 
-  return acc;
-}, {} as Record<typeof queriesKeys[number], string>);
+export const useBreakpoints = () => {
+  const breakpoints: Record<(typeof queriesKeys)[number], boolean> = {
+    des1920: useIctinusMediaQuery('des1920'),
+    des1440: useIctinusMediaQuery('des1440'),
+    des1366: useIctinusMediaQuery('des1366'),
+    des1200: useIctinusMediaQuery('des1200'),
+    tab1024: useIctinusMediaQuery('tab1024'),
+    tab970: useIctinusMediaQuery('tab970'),
+    tab750: useIctinusMediaQuery('tab750'),
+    mob480: useIctinusMediaQuery('mob480'),
+    mob320: useIctinusMediaQuery('mob320'),
+  };
 
-const useBreakpoints = () => {
-  return useMedia<Record<typeof queriesKeys[number], string>>({ queries });
+  return breakpoints;
 };
 
 export default useBreakpoints;
