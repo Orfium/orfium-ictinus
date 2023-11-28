a687ef394a7491e826c17ec52c0cf05b2defc373
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2023-11-28 09:35:03 +0200

    feat: mdx to csf

---
+++ src/components/TopAppBar/TopAppBar.stories.tsx
@@ -0,0 +1,80 @@
+import TopAppBar from './TopAppBar';
+import TopAppBarShowcase from '../storyUtils/TopAppBarShowcase';
+import { boolean } from '@storybook/addon-knobs';
+import { FIGMA_URL } from '../../utils/common';
+
+export default {
+  title: 'Original Components/TopNavBar',
+  component: TopAppBar,
+
+  parameters: {
+    design: [
+      {
+        type: 'figma',
+        url: `${FIGMA_URL}?node-id=1213%3A74735`,
+      },
+    ],
+  },
+};
+
+export const WithLogoPlaceholder = {
+  render: () => (
+    <div
+      style={{
+        padding: 10,
+      }}
+    >
+      <TopAppBarShowcase />
+    </div>
+  ),
+
+  name: 'with Logo Placeholder',
+};
+
+export const WithAdditionalTools = {
+  render: () => (
+    <div
+      style={{
+        padding: 10,
+      }}
+    >
+      <TopAppBarShowcase hasAdditionalTools={true} />
+    </div>
+  ),
+
+  name: 'with Additional Tools',
+};
+
+export const WithDarkThemeEnabled = {
+  render: () => (
+    <div
+      style={{
+        padding: 10,
+      }}
+    >
+      <TopAppBarShowcase isDark={true} />
+    </div>
+  ),
+
+  name: 'with Dark theme enabled',
+};
+
+export const Playground = {
+  render: () => (
+    <div
+      style={{
+        padding: 10,
+      }}
+    >
+      <TopAppBarShowcase
+        isDark={boolean('isDark', false)}
+        isSearchDisabled={boolean('disabled search', false)}
+        hasSearchHandler={boolean('hasSearchHandler', false)}
+        hasAdditionalTools={boolean('hasAdditionalTools', false)}
+        hasLogo={boolean('hasLogo', false)}
+      />
+    </div>
+  ),
+
+  name: 'playground',
+};
---
+++ src/hooks/useSearchQueryParams.stories.tsx
@@ -0,0 +1,33 @@
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { MemoryRouter } from 'react-router-dom';
+import DemoUseSearchQueryParams from './storyUtils/DemoUseSearchQueryParams';
+
+export default {
+  title: 'Hooks/useSearchQueryParams',
+  component: () => true,
+};
+
+export const UseSearchQueryParams = {
+  name: 'useSearchQueryParams',
+
+  parameters: {
+    query: {
+      q: 'search',
+    },
+    chromatic: { disableSnapshot: true },
+  },
+
+  decorators: [
+    withKnobs,
+    () => {
+      const value = text('given url', '/test?page=1&size=12&library=react&testing=yolo');
+
+      return (
+        <MemoryRouter initialEntries={[value]}>
+          {/*// @ts-ignore*/}
+          <DemoUseSearchQueryParams initialEntries={value} />
+        </MemoryRouter>
+      );
+    },
+  ],
+};
