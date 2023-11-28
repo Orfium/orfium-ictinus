a6db2ee40c1f399275c9829ebb3e8484b7564919
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2023-11-28 09:34:10 +0200

    feat: mdx to csf

---
+++ src/components/Avatar/AvatarStack.stories.tsx
@@ -0,0 +1,140 @@
+import Avatar from './Avatar';
+import AvatarStack from './AvatarStack';
+import Stack from '../storyUtils/Stack';
+import { FIGMA_URL } from '../../utils/common';
+
+export default {
+  title: 'Updated Components/Avatar/AvatarStack',
+  component: Avatar,
+
+  parameters: {
+    design: [
+      {
+        type: 'figma',
+        name: 'Avatar',
+        url: `${FIGMA_URL}?node-id=3325%3A58246`,
+      },
+    ],
+  },
+};
+
+export const AvatarStackStory = {
+  render: () => (
+    <Stack isVertical>
+      <AvatarStack size={1} color={'blue'}>
+        <Avatar size={1} src={'https://mui.com/static/images/avatar/6.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={1} src={'https://mui.com/static/images/avatar/5.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={1} color={'blue'}>
+          JN
+        </Avatar>
+        <Avatar size={1} src={'https://mui.com/static/images/avatar/3.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={1} src={'https://mui.com/static/images/avatar/2.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={1} src={'https://mui.com/static/images/avatar/1.jpg'}>
+          JN
+        </Avatar>
+      </AvatarStack>
+      <AvatarStack size={2} color={'blue'}>
+        <Avatar size={2} src={'https://mui.com/static/images/avatar/6.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={2} src={'https://mui.com/static/images/avatar/5.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={2} color={'blue'}>
+          JN
+        </Avatar>
+        <Avatar size={2} src={'https://mui.com/static/images/avatar/3.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={2} src={'https://mui.com/static/images/avatar/2.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={2} src={'https://mui.com/static/images/avatar/1.jpg'}>
+          JN
+        </Avatar>
+      </AvatarStack>
+      <AvatarStack size={3} maxAvatars={3} color={'blue'}>
+        <Avatar size={3} src={'https://mui.com/static/images/avatar/6.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={3} color={'blue'} />
+        <Avatar size={3} src={'https://mui.com/static/images/avatar/4.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={3} src={'https://mui.com/static/images/avatar/3.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={3} src={'https://mui.com/static/images/avatar/2.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={3} src={'https://mui.com/static/images/avatar/1.jpg'}>
+          JN
+        </Avatar>
+      </AvatarStack>
+      <AvatarStack size={4} color={'blue'}>
+        <Avatar size={4} src={'https://mui.com/static/images/avatar/6.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={4} src={'https://mui.com/static/images/avatar/5.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={4} src={'https://mui.com/static/images/avatar/4.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={4} src={'https://mui.com/static/images/avatar/3.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={4} src={'https://mui.com/static/images/avatar/2.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={4} src={'https://mui.com/static/images/avatar/1.jpg'}>
+          JN
+        </Avatar>
+      </AvatarStack>
+      <AvatarStack size={5} maxAvatars={3} color={'blue'}>
+        <Avatar size={5} src={'https://mui.com/static/images/avatar/6.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={5} src={'https://mui.com/static/images/avatar/4.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={5} src={'https://mui.com/static/images/avatar/3.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={5} src={'https://mui.com/static/images/avatar/2.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={5} src={'https://mui.com/static/images/avatar/1.jpg'}>
+          JN
+        </Avatar>
+      </AvatarStack>
+      <AvatarStack size={6} maxAvatars={3} color={'blue'}>
+        <Avatar size={6} src={'https://mui.com/static/images/avatar/6.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={6} src={'https://mui.com/static/images/avatar/4.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={6} src={'https://mui.com/static/images/avatar/3.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={6} src={'https://mui.com/static/images/avatar/2.jpg'}>
+          JN
+        </Avatar>
+        <Avatar size={6} src={'https://mui.com/static/images/avatar/1.jpg'}>
+          JN
+        </Avatar>
+      </AvatarStack>
+    </Stack>
+  ),
+
+  name: 'Avatar Stack',
+};
---
+++ src/components/List/stories/List.stories.tsx
@@ -0,0 +1,88 @@
+import List, { ListSection, ListItemText, ListItem } from '../index';
+import { FIGMA_URL, Function } from '../../../utils/common';
+
+export default {
+  title: 'Updated Components/List/List',
+  component: List,
+  parameters: {
+    design: [
+      {
+        type: 'figma',
+        name: 'List',
+        url: `${FIGMA_URL}?node-id=22406-71589`,
+      },
+    ],
+  },
+};
+
+export const ListStory = {
+  render: () => (
+    <Function>
+      {() => {
+        return (
+          <List label={'list'}>
+            {new Array(7).fill(undefined).map((__, index) => {
+              return (
+                <ListItem key={`Item ${index}`}>
+                  <ListItemText>Item{index}</ListItemText>
+                </ListItem>
+              );
+            })}
+          </List>
+        );
+      }}
+    </Function>
+  ),
+
+  name: 'List',
+};
+
+export const GroupList = {
+  render: () => (
+    <Function>
+      {/* @TODO */}
+      {() => {
+        return (
+          <List label={'group list'}>
+            {new Array(3).fill(undefined).map((__, index) => {
+              return (
+                <ListSection key={`Group_${index}`} title={`Group ${index}`}>
+                  <ListItem key={`Group_${index}_1`}>
+                    <ListItemText>Item 1</ListItemText>
+                  </ListItem>
+                  <ListItem key={`Group_${index}_2`}>
+                    <ListItemText>Item 2</ListItemText>
+                  </ListItem>
+                </ListSection>
+              );
+            })}
+          </List>
+        );
+      }}
+    </Function>
+  ),
+
+  name: 'Group List',
+};
+
+export const VirtualizedList = {
+  render: () => (
+    <Function>
+      {() => {
+        return (
+          <List label={'list'} height={450} isVirtualized={true}>
+            {new Array(1000).fill(undefined).map((__, index) => {
+              return (
+                <ListItem key={`Item ${index}`}>
+                  <ListItemText>Item {index}</ListItemText>
+                </ListItem>
+              );
+            })}
+          </List>
+        );
+      }}
+    </Function>
+  ),
+
+  name: 'Virtualized List',
+};
