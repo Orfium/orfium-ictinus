815bb231c9f7f39eaadd3eaed64b96b931d910e7
kostasdano <kostas.danochristos@orfium.com>
2024-01-16 14:59:17 +0200

    fix: filter hover

--- src/components/Search/Search.style.ts
+++ src/components/Search/Search.style.ts
@@ -43,7 +43,7 @@ export const filterStyles =
         position: relative;
         z-index: 0;
 
-        &:hover:enabled {
+        &:hover[data-active="false"]:enabled {
           background: ${tokens('backgroundColor.focused')};
         }
 
