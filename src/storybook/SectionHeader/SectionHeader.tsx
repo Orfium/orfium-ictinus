a9640a1717bd7e24680f0b0f163409b44e607e22
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2024-01-31 16:14:34 +0200

    docs: shaping up all props and styles of old and new

--- src/storybook/SectionHeader/SectionHeader.tsx
+++ src/storybook/SectionHeader/SectionHeader.tsx
@@ -3,17 +3,25 @@ import type { FCC } from 'react';
 import React from 'react';
 
 import { sectionHeaderWrapper } from './SectionHeader.style';
+import Box from '../../components/Box';
 import Typography from '../Typography';
+import Tag from 'components/Tag';
 
 export type SectionHeaderProps = {
   title?: string;
+  isPending?: boolean;
 };
 
-const SectionHeader: FCC<SectionHeaderProps> = ({ title = '' }) => {
+const SectionHeader: FCC<SectionHeaderProps> = ({ title = '', isPending = false }) => {
   return (
     <Unstyled>
       <div css={sectionHeaderWrapper()}>
         <Typography variant="headline01">{title}</Typography>
+        {isPending && (
+          <Box ml="6">
+            <Tag>v4</Tag>
+          </Box>
+        )}
       </div>
     </Unstyled>
   );
