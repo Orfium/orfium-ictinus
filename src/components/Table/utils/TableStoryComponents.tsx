d1f8ba6ac132638dc6ac800400a6d4a37b26b1ea
panagiotis vourtsis <vourtsis_pan@hotmail.com>
2024-02-22 10:11:31 +0200

    docs(table): adding all small types storybook missed

--- src/components/Table/utils/TableStoryComponents.tsx
+++ src/components/Table/utils/TableStoryComponents.tsx
@@ -1,9 +1,13 @@
 import type React from 'react';
 
 import type { Cell, Row } from '../Table';
+import type { ExtendedColumn } from '../types';
 
 /** This is just a dummy component to be used in Storybook for showing the Row type on props */
 export const RowStory: React.FC<Row<unknown>> = () => null;
 
-/** This is just a dummy component to be used in Storybook for showing the Row type on props */
+/** This is just a dummy component to be used in Storybook for showing the Cell type on props */
 export const CellStory: React.FC<Cell<unknown>> = () => null;
+
+/** This is just a dummy component to be used in Storybook for showing the ExtendedColumn type on props */
+export const ExtendedColumnStory: React.FC<ExtendedColumn> = () => null;
