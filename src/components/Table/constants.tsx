eb90b193d1e0cb589bcdc4593654ae0fb594059c
kostasdano <kostas.danochristos@orfium.com>
2024-04-29 16:17:17 +0300

    feat: documentation

--- src/components/Table/constants.tsx
+++ src/components/Table/constants.tsx
@@ -61,3 +61,78 @@ export const simpleData = [
     },
   },
 ];
+
+export const moreData = [
+  {
+    cells: {
+      firstName: 'Gunther',
+      lastName: 'N/A',
+      age: '35',
+      job: 'Central Perk Manager',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Janice',
+      lastName: 'Litman-Foralnik',
+      age: '35',
+      job: 'Personal Shopper',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Richard',
+      lastName: 'Burke',
+      age: '50',
+      job: 'Ophthalmologist',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Estelle',
+      lastName: 'Leonard',
+      age: 'N/A',
+      job: 'Talent Agent',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Mike',
+      lastName: 'Hannigan',
+      age: '31',
+      job: 'N/A',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Charlie',
+      lastName: 'Wheeler',
+      age: '28',
+      job: 'Paleontologist',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Emily',
+      lastName: 'Waltham',
+      age: '28',
+      job: 'N/A',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Carol',
+      lastName: 'Willick',
+      age: '35',
+      job: 'N/A',
+    },
+  },
+  {
+    cells: {
+      firstName: 'Frank',
+      lastName: 'Buffay Jr.',
+      age: '29',
+      job: 'N/A',
+    },
+  },
+];
