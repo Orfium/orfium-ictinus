ff5a4efb46b2b61f2b031e1c74ef790fe2bdc151
George Karalis <15313012+geokaralis@users.noreply.github.com>
2025-06-04 12:02:17 +0300

    feat: toast v5 (#856)
    * feat: toast aria
    
    * chore: check mdx
    
    * chore: add stories
    
    * feat: toast tests
    
    * chore: align docs
    
    * fix: add hasIcon

--- src/components/ThemeProvider/ThemeProvider.style.ts
+++ src/components/ThemeProvider/ThemeProvider.style.ts
@@ -71,4 +71,31 @@ export const globalStyles = (theme: Theme): SerializedStyles => css`
   *:focus {
     outline: 0;
   }
+
+  ::view-transition-group(*) {
+    animation-duration: 250ms;
+    /* swift out */
+    animation-timing-function: cubic-bezier(0.175, 0.885, 0.32, 1.1);
+  }
+
+  ::view-transition-new(.toast):only-child {
+    animation-name: fade-in-up;
+  }
+
+  ::view-transition-old(.toast):only-child {
+    animation-name: fade-out;
+  }
+
+  @keyframes fade-out {
+    to {
+      opacity: 0;
+    }
+  }
+
+  @keyframes fade-in-up {
+    from {
+      opacity: 0;
+      transform: translateY(24px);
+    }
+  }
 `;
---
+++ src/components/ToastV4/ToastV4.test.tsx
@@ -0,0 +1,67 @@
+import { css } from '@emotion/react';
+import { fireEvent, render } from '../../test';
+import Toast from './ToastV4';
+
+describe('Generic Toast', () => {
+  const data = {
+    message: 'message',
+  };
+
+  test('Generic Toast with simple visual renders correctly', async () => {
+    const closeCTA = vi.fn();
+
+    const { findByText } = render(
+      <Toast {...data} closeCTA={closeCTA}>
+        <div
+          css={css`
+            width: 1024px;
+            height: 768px;
+          `}
+        >
+          <h1>container-test-data</h1>
+        </div>
+      </Toast>
+    );
+
+    const message = await findByText(data.message);
+    expect(message).toBeTruthy();
+  });
+
+  test('Generic Toast with simple visual expands/shrinks correctly', async () => {
+    const closeCTA = vi.fn();
+
+    const { findByTestId, findByText } = render(
+      <Toast {...data} closeCTA={closeCTA}>
+        <h1>container-test-data</h1>
+      </Toast>
+    );
+
+    // Toast is initialized with isExpanded=false
+    // Note: The visual is rendered regardless of the isExpanded value, so it can be always found in document
+    // If isExpanded=false, the min-height of the visual is 0, else > 0.
+    // So to test if the visual is visible or not, the only way to check it is check the min-height
+
+    const expandButton = await findByTestId('toast-expand');
+    const expandedContainer = await findByTestId('expanded-container');
+    expect(expandedContainer).toHaveStyle('min-height: 0rem;');
+    fireEvent.click(expandButton);
+    expect(expandedContainer).toHaveStyle('min-height: 9.125rem;');
+
+    const containerText = await findByText('container-test-data');
+    expect(containerText).toBeTruthy();
+  });
+
+  test('Generic Toast with simple visual closeCTA works properly', async () => {
+    const closeCTA = vi.fn();
+
+    const { findByTestId } = render(
+      <Toast {...data} closeCTA={closeCTA}>
+        <h1>container-test-data</h1>
+      </Toast>
+    );
+
+    const closeButton = await findByTestId('toast-close');
+    fireEvent.click(closeButton);
+    expect(closeCTA).toHaveBeenCalledTimes(1);
+  });
+});
