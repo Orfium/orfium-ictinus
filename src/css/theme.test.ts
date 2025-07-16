7889e3b64ee306aa0ef4570d904dd3ec9a1fce1c
George Karalis <15313012+geokaralis@users.noreply.github.com>
2025-07-16 15:36:53 +0300

    chore: test deploy

--- src/css/theme.test.ts
+++ src/css/theme.test.ts
@@ -1,24 +1,24 @@
-import { createGlobalTheme } from '@vanilla-extract/css';
-import { darkSemanticColors, globalColors, lightSemanticColors } from '~/tokens/color';
-import { space } from '~/tokens/space';
-import { vars } from './vars.css';
+// import { createGlobalTheme } from '@vanilla-extract/css';
+// import { darkSemanticColors, globalColors, lightSemanticColors } from '~/tokens/color';
+// import { space } from '~/tokens/space';
+// import { vars } from './vars.css';
 
-const theme = {
-  color: {
-    ...globalColors,
-    ...lightSemanticColors,
-  },
-  space,
-};
+// const theme = {
+//   color: {
+//     ...globalColors,
+//     ...lightSemanticColors,
+//   },
+//   space,
+// };
 
-const darkTheme = {
-  ...globalColors,
-  ...darkSemanticColors,
-};
+// const darkTheme = {
+//   ...globalColors,
+//   ...darkSemanticColors,
+// };
 
-createGlobalTheme(':root', vars, { '@layer': 'ictinus-theme', ...theme });
+// createGlobalTheme(':root', vars, { '@layer': 'ictinus-theme', ...theme });
 
-createGlobalTheme(':root.dark', vars.color, {
-  '@layer': 'ictinus-theme',
-  ...darkTheme,
-});
+// createGlobalTheme(':root.dark', vars.color, {
+//   '@layer': 'ictinus-theme',
+//   ...darkTheme,
+// });
