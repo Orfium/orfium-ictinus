bb1a579c0ac9031453175ce42bc138e6dce339e3
George Karalis <15313012+geokaralis@users.noreply.github.com>
2025-10-13 12:37:57 +0300

    feat: extend box in tooltip content

--- packages/ictinus/src/vanilla/Tooltip/Tooltip.tsx
+++ packages/ictinus/src/vanilla/Tooltip/Tooltip.tsx
@@ -9,7 +9,6 @@ import {
 } from 'react';
 import type { TooltipProps as TooltipPrimitiveProps } from 'react-aria-components';
 import {
-  composeRenderProps,
   Focusable,
   OverlayArrow,
   Tooltip as TooltipPrimitive,
@@ -17,6 +16,8 @@ import {
 } from 'react-aria-components';
 
 import { cn } from '../../utils/cn';
+import { type ExtendProps } from '../../utils/ExtendProps';
+import { Box, extractBoxProps, type BoxProps } from '../Box';
 import * as styles from './Tooltip.css';
 
 const TooltipContext = createContext<{
@@ -68,55 +69,57 @@ const Tooltip = ({
 
 Tooltip.displayName = 'Tooltip';
 
-interface TooltipContentProps
-  extends Omit<TooltipPrimitiveProps, 'children'>,
-    NonNullable<styles.TooltipVariants> {
-  showArrow?: boolean;
-  children?: ReactNode;
-}
+type TooltipContentProps = Omit<TooltipPrimitiveProps, 'children'> &
+  ExtendProps<BoxProps, NonNullable<styles.TooltipVariants>> & {
+    showArrow?: boolean;
+    children?: ReactNode;
+  };
 
 const TooltipContent = ({
   offset = 8,
   inverse = false,
   showArrow = true,
   children,
+  className,
   ...props
 }: TooltipContentProps) => {
+  const { boxProps, restProps } = extractBoxProps(props);
+
   return (
-    <TooltipPrimitive
-      {...props}
-      offset={offset}
-      className={composeRenderProps(props.className, (className) =>
-        cn(styles.tooltip({ inverse }), className)
-      )}
-    >
-      {showArrow && (
-        <OverlayArrow>
-          {inverse ? (
-            <svg width={18} height={8} viewBox="0 0 18 8" className={styles.arrowInverse()}>
-              <path d="M9.00744 6.52304L1.6397 0.000488281L16.3752 0.000489658L9.00744 6.52304Z" />
-              <path
-                d="M9.00337 6.51095L16.3588 0.00158653H18.0034L9.00337 8.00049L0.00337219 0.00158653L1.6397 0.000488281L9.00337 6.51095Z"
-                className={styles.arrowInverseBorder}
-              />
-            </svg>
-          ) : (
-            <svg width={15} height={6} viewBox="0 0 15 6" className={styles.arrow()}>
-              <path d="M7.5 6L0.500001 -1.22392e-06L14.5 0L7.5 6Z" />
-            </svg>
-          )}
-        </OverlayArrow>
-      )}
-      {children}
-    </TooltipPrimitive>
+    <Box asChild className={cn(styles.tooltip({ inverse }), className)} {...boxProps}>
+      <TooltipPrimitive {...restProps} offset={offset}>
+        {showArrow && (
+          <OverlayArrow>
+            {inverse ? (
+              <svg width={18} height={8} viewBox="0 0 18 8" className={styles.arrowInverse()}>
+                <path d="M9.00744 6.52304L1.6397 0.000488281L16.3752 0.000489658L9.00744 6.52304Z" />
+                <path
+                  d="M9.00337 6.51095L16.3588 0.00158653H18.0034L9.00337 8.00049L0.00337219 0.00158653L1.6397 0.000488281L9.00337 6.51095Z"
+                  className={styles.arrowInverseBorder}
+                />
+              </svg>
+            ) : (
+              <svg width={15} height={6} viewBox="0 0 15 6" className={styles.arrow()}>
+                <path d="M7.5 6L0.500001 -1.22392e-06L14.5 0L7.5 6Z" />
+              </svg>
+            )}
+          </OverlayArrow>
+        )}
+        {children}
+      </TooltipPrimitive>
+    </Box>
   );
 };
+
+TooltipContent.displayName = 'TooltipContent';
 
 function TooltipTrigger({ ...props }: ComponentProps<typeof Focusable>) {
   const { triggerRef } = useContext(TooltipContext);
 
   return <Focusable ref={triggerRef} data-slot="tooltip-trigger" {...props} />;
 }
+
+TooltipTrigger.displayName = 'TooltipTrigger';
 
 const hasTruncatedContent = (element: HTMLElement) => {
   let truncated = false;
